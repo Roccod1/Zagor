35ac7101f4cbf9c69889b0bd845e045e3b5b1f32
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-06-24 17:29:05 +0200

    Initial Commit

---
+++ modules/gestione-enti/gestione-enti-frontend/src/main/java/it/servizidigitali/gestioneenti/frontend/constants/GestioneEntiPortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.gestioneenti.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class GestioneEntiPortletKeys {
+
+	public static final String GESTIONEENTI =
+		"it_servizidigitali_gestioneenti_frontend_GestioneEntiPortlet";
+
+}
---
+++ modules/gestione-servizi/gestione-servizi-frontend/src/main/java/it/servizidigitali/gestioneservizi/frontend/constants/GestioneServiziPortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.gestioneservizi.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class GestioneServiziPortletKeys {
+
+	public static final String GESTIONESERVIZI =
+		"it_servizidigitali_gestioneservizi_frontend_GestioneServiziPortlet";
+
+}
