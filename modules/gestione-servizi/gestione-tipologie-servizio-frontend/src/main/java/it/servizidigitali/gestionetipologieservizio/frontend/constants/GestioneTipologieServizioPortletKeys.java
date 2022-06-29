3bed3b5e8937974c122c706a04cb8a69e8f3b8e7
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-06-29 10:19:48 +0200

    Aggiunto scheletro portlet gestione tipologie servizio.

---
+++ modules/gestione-servizi/gestione-tipologie-servizio-frontend/src/main/java/it/servizidigitali/gestionetipologieservizio/frontend/constants/GestioneTipologieServizioPortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.gestionetipologieservizio.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class GestioneTipologieServizioPortletKeys {
+
+	public static final String GESTIONETIPOLOGIESERVIZIO =
+		"it_servizidigitali_gestionetipologieservizio_frontend_GestioneTipologieServizioPortlet";
+
+}
