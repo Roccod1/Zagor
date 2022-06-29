3af3fc9ac302cb58da947c4154fd7efcd1e3c30b
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-06-29 10:19:32 +0200

    Aggiunto scheletro portlet gestione aree tematiche.

---
+++ modules/gestione-servizi/gestione-aree-tematiche-frontend/src/main/java/it/servizidigitali/gestioneareetematiche/frontend/constants/GestioneAreeTematichePortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.gestioneareetematiche.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class GestioneAreeTematichePortletKeys {
+
+	public static final String GESTIONEAREETEMATICHE =
+		"it_servizidigitali_gestioneareetematiche_frontend_GestioneAreeTematichePortlet";
+
+}
---
+++ modules/gestione-servizi/gestione-aree-tematiche-frontend/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="gestioneareetematiche.caption"/></b>
+</p>
