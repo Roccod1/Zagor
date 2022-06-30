7ff46b37efbbbfbe743f36ce362ae5af197cdf6e
COSTABILEE <COSTABILEE@COSTABILEE-NB>
2022-06-30 18:03:17 +0200

    implementazioni

--- modules/gestione-forms/gestione-forms-frontend/src/main/java/it/servizidigitali/gestioneforms/frontend/portlet/GestioneFormsPortlet.java
+++ modules/gestione-forms/gestione-forms-frontend/src/main/java/it/servizidigitali/gestioneforms/frontend/portlet/GestioneFormsPortlet.java
@@ -1,12 +1,26 @@
 package it.servizidigitali.gestioneforms.frontend.portlet;
 
 import it.servizidigitali.gestioneforms.frontend.constants.GestioneFormsPortletKeys;
+import it.servizidigitali.gestioneforms.model.Form;
+import it.servizidigitali.gestioneforms.service.FormLocalService;
+import it.servizidigitali.gestioneforms.service.FormLocalServiceUtil;
+import it.servizidigitali.gestioneforms.service.persistence.FormUtil;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author pindi
@@ -27,4 +41,12 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class GestioneFormsPortlet extends MVCPortlet {
+	@Reference
+	private FormLocalService formLocalService;
+	
+	public void render (RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException{
+		
+		renderRequest.setAttribute("listaForm", formLocalService.getForms(QueryUtil.ALL_POS, QueryUtil.ALL_POS));
+		super.render(renderRequest, renderResponse);
+	}
 }
