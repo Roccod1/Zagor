3c7d99242a864f0a59f046f00b167680ef25678a
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-07-06 09:28:47 +0200

    Eliminati riferimenti Service Builder non necessari.

--- modules/gestione-servizi/gestione-servizi-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,8 +0,0 @@
-create index IX_3FDA0500 on ServiziDigitaliGestioneServizi_servizio_tipologia (companyId);
-create index IX_697CBA2B on ServiziDigitaliGestioneServizi_servizio_tipologia (tipologiaId);
-
-create index IX_A2FAC60D on _servizio_tipologia (companyId);
-create index IX_87716BF8 on _servizio_tipologia (tipologiaId);
-
-create index IX_1913124C on servizio_tipologia (companyId);
-create index IX_D8A7A477 on servizio_tipologia (tipologiaId);
