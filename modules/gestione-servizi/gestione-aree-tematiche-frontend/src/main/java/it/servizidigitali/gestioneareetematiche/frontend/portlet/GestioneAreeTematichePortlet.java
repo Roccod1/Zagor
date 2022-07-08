81baf2c855365243864d8f512f4169aff41e08cb
filierim <marco.filieri@linksmt.it>
2022-07-08 09:27:08 +0200

    Fix nome jsp, aggiunta getAll al render della portlet per visualizzare i risultati nel search container

--- modules/gestione-servizi/gestione-aree-tematiche-frontend/src/main/java/it/servizidigitali/gestioneareetematiche/frontend/portlet/GestioneAreeTematichePortlet.java
+++ modules/gestione-servizi/gestione-aree-tematiche-frontend/src/main/java/it/servizidigitali/gestioneareetematiche/frontend/portlet/GestioneAreeTematichePortlet.java
@@ -1,11 +1,13 @@
 package it.servizidigitali.gestioneareetematiche.frontend.portlet;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.JavaConstants;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletConfig;
@@ -14,8 +16,11 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import it.servizidigitali.gestioneareetematiche.frontend.constants.GestioneAreeTematichePortletKeys;
+import it.servizidigitali.gestioneservizi.model.AreaTematica;
+import it.servizidigitali.gestioneservizi.service.AreaTematicaLocalService;
 
 /**
  * @author pindi
@@ -23,7 +28,7 @@ import it.servizidigitali.gestioneareetematiche.frontend.constants.GestioneAreeT
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.servizidigitali",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=GestioneAreeTematiche",
@@ -36,10 +41,17 @@ import it.servizidigitali.gestioneareetematiche.frontend.constants.GestioneAreeT
 	service = Portlet.class
 )
 public class GestioneAreeTematichePortlet extends MVCPortlet {
+	
+	@Reference
+	private AreaTematicaLocalService areaTematicaLocalService;
+	
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
 		
-
+		List<AreaTematica> listaAreeTematiche = areaTematicaLocalService.getAreaTematicas(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		
+		renderRequest.setAttribute(GestioneAreeTematichePortletKeys.LISTA_AREE_TEMATICHE, listaAreeTematiche);
+		
 		//nascondo messaggi success e error default
 		PortletConfig portletConfig = (PortletConfig) renderRequest.getAttribute(JavaConstants.JAVAX_PORTLET_CONFIG);
 		LiferayPortletConfig liferayPortletConfig = (LiferayPortletConfig) portletConfig;
