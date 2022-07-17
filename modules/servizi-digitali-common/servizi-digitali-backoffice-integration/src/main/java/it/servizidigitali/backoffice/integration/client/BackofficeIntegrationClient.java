b25e9748d51aa4dbef13e001db38544436019e91
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-07-17 11:47:55 +0200

    Implementato client Axis2 verso integration-ws.

--- modules/servizi-digitali-common/servizi-digitali-backoffice-integration/src/main/java/it/servizidigitali/backoffice/integration/client/BackofficeIntegrationClient.java
+++ modules/servizi-digitali-common/servizi-digitali-backoffice-integration/src/main/java/it/servizidigitali/backoffice/integration/client/BackofficeIntegrationClient.java
@@ -3,14 +3,14 @@ package it.servizidigitali.backoffice.integration.client;
 import it.servizidigitali.backoffice.integration.exception.BackofficeIntegrationClientException;
 
 /**
- * 
+ *
  * @author pindi
  *
  */
 public interface BackofficeIntegrationClient {
 
 	/**
-	 * 
+	 *
 	 * @param payloadXml
 	 * @param nomeServizio
 	 * @param wsUrl
@@ -18,6 +18,6 @@ public interface BackofficeIntegrationClient {
 	 * @return
 	 * @throws BackofficeIntegrationClientException
 	 */
-	String sendXml(String payloadXml, String nomeServizio, String wsUrl, int... readTimeout) throws BackofficeIntegrationClientException;
+	String sendXml(String payloadXml, String nomeServizio, String wsUrl, Integer readTimeout) throws BackofficeIntegrationClientException;
 
 }
--- modules/servizi-digitali-common/servizi-digitali-backoffice-integration/src/main/java/it/servizidigitali/backoffice/integration/client/impl/BackofficeIntegrationClientImpl.java
+++ modules/servizi-digitali-common/servizi-digitali-backoffice-integration/src/main/java/it/servizidigitali/backoffice/integration/client/impl/BackofficeIntegrationClientImpl.java
@@ -3,20 +3,18 @@ package it.servizidigitali.backoffice.integration.client.impl;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-
-import org.apache.axis.client.Service;
+import org.apache.axis.client.Stub;
 import org.osgi.service.component.annotations.Component;
 
 import it.linksmt.servizionline.ws.PayloadXmlRequest;
-import it.linksmt.servizionline.ws.ServiziOnlinePortSoap11Stub;
+import it.linksmt.servizionline.ws.PayloadXmlResponse;
+import it.linksmt.servizionline.ws.ServiziOnlinePort;
+import it.linksmt.servizionline.ws.ServiziOnlinePortServiceLocator;
 import it.servizidigitali.backoffice.integration.client.BackofficeIntegrationClient;
 import it.servizidigitali.backoffice.integration.exception.BackofficeIntegrationClientException;
 
 /**
- * 
+ *
  * @author pindi
  *
  */
@@ -26,21 +24,27 @@ public class BackofficeIntegrationClientImpl implements BackofficeIntegrationCli
 	private static final Log log = LogFactoryUtil.getLog(BackofficeIntegrationClientImpl.class.getName());
 
 	@Override
-	public String sendXml(String payloadXml, String nomeServizio, String wsUrl, int... readTimeout) throws BackofficeIntegrationClientException {
+	public String sendXml(String payloadXml, String nomeServizio, String wsUrl, Integer readTimeout) throws BackofficeIntegrationClientException {
 		try {
-			URL url = new URL(wsUrl);
-			QName qname = new QName("http://ws.servizionline.linksmt.it", "PayloadXml");
-			Service service = new Service(url, qname);
-			ServiziOnlinePortSoap11Stub serviziOnlinePortSoap11Stub = new ServiziOnlinePortSoap11Stub(service);
 
+			ServiziOnlinePortServiceLocator locator = new ServiziOnlinePortServiceLocator();
+			locator.setServiziOnlinePortSoap11EndpointAddress(wsUrl);
 			PayloadXmlRequest payloadXmlRequest = new PayloadXmlRequest(nomeServizio, payloadXml);
-			serviziOnlinePortSoap11Stub.payloadXml(payloadXmlRequest);
+			ServiziOnlinePort serviziOnlinePortSoap11 = locator.getServiziOnlinePortSoap11();
+
+			if (readTimeout != null) {
+				org.apache.axis.client.Stub stub = (Stub) serviziOnlinePortSoap11;
+				stub.setTimeout(readTimeout);
+			}
+
+			PayloadXmlResponse payloadXmlResponse = serviziOnlinePortSoap11.payloadXml(payloadXmlRequest);
+
+			return payloadXmlResponse.getXml();
 		}
 		catch (Exception e) {
 			log.error("sendXml :: " + e.getMessage(), e);
 			throw new BackofficeIntegrationClientException(e.getMessage(), e);
 		}
-		return null;
 	}
 
 }
