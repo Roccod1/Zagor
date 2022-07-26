d4495afcf2a5596077c0e3fac30071f26d6acbe2
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-07-26 18:27:07 +0200

    Aggiornata api Base Path di default Camunda.

--- modules/servizi-digitali-common/servizi-digitali-camunda-integration/src/main/java/it/servizidigitali/camunda/integration/configuration/CamundaConfiguration.java
+++ modules/servizi-digitali-common/servizi-digitali-camunda-integration/src/main/java/it/servizidigitali/camunda/integration/configuration/CamundaConfiguration.java
@@ -10,7 +10,7 @@ import aQute.bnd.annotation.metatype.Meta;
 @Meta.OCD(id = "it.servizidigitali.camunda.integration.configuration.CamundaConfiguration", name = "Configurazioni Camunda")
 public interface CamundaConfiguration {
 
-	@Meta.AD(deflt = "http://servizi-digitali-dev:8082", required = false, description = "Path API Camunda")
+	@Meta.AD(deflt = "http://servizi-digitali-dev:8082/engine-rest", required = false, description = "Path API Camunda")
 	String apiBasePath();
 
 	@Meta.AD(deflt = "admin", required = false, description = "Username accesso API Camunda")
