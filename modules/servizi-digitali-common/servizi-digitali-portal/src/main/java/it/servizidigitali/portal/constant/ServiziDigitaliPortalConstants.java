fc3c9be02b0dfa8de95c6e721f138a5044514933
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-07-29 13:52:04 +0200

    Aggiunte LifecycleAction per pulizia dati backoffice da cache.

---
+++ modules/servizi-digitali-common/servizi-digitali-portal/src/main/java/it/servizidigitali/portal/constant/ServiziDigitaliPortalConstants.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.portal.constant;
+
+/**
+ * @author pindi
+ *
+ */
+public interface ServiziDigitaliPortalConstants {
+
+	static String CURRENT_USER_SCREEN_NAME = "CURRENT_USER_SCREEN_NAME";
+
+}
