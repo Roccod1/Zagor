cd5f73a5a9f27118df0de344288930d9acd4a8f6
Zonnog <giacomo.zonno@linksmt.it>
2022-08-10 11:51:31 +0200

    Step checkbox riepilogo allegati interno all'alpaca form, recuperato e settato dinamicamente

--- modules/presentatore-forms/presentatore-forms-frontend/src/main/java/it/servizidigitali/presentatoreforms/frontend/constants/PresentatoreFormsPortletKeys.java
+++ modules/presentatore-forms/presentatore-forms-frontend/src/main/java/it/servizidigitali/presentatoreforms/frontend/constants/PresentatoreFormsPortletKeys.java
@@ -7,5 +7,7 @@ public class PresentatoreFormsPortletKeys {
 
 	public static final String PRESENTATOREFORMS =
 		"it_servizidigitali_presentatoreforms_frontend_PresentatoreFormsPortlet";
+	
+	public static final String ALPACA_STRUCTURE = "alpacaStructure";
 
 }
