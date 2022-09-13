74e296967ae624367eb3f3321d68c4efea2aa519
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-13 17:36:07 +0200

    Fattorizzate enumeration comuni per configurazione procedure.

---
+++ modules/servizi-digitali-common/servizi-digitali-common-utility/src/main/java/it/servizidigitali/common/utility/enumeration/TipoServizio.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.common.utility.enumeration;
+
+/**
+ * @author pindi
+ *
+ */
+public enum TipoServizio {
+
+	DICHIARAZIONE, AUTO_DICHIARAZIONE, CERTIFICATO, VISURA, PAGAMENTO, CONCORSO;
+
+}
