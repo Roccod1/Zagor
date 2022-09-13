e309ba8e304bcc492bff0bc6a413f1a3aa375925
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-13 16:20:03 +0200

    Fix chiamata servizio claim by screenName.

--- modules/scrivania-operatore/scrivania-operatore-frontend/src/main/java/it/servizidigitali/scrivaniaoperatore/frontend/portlet/action/PrendiInCaricoActionCommand.java
+++ modules/scrivania-operatore/scrivania-operatore-frontend/src/main/java/it/servizidigitali/scrivaniaoperatore/frontend/portlet/action/PrendiInCaricoActionCommand.java
@@ -4,9 +4,9 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -17,25 +17,23 @@ import org.osgi.service.component.annotations.Reference;
 import it.servizidigitali.scrivaniaoperatore.frontend.constants.ScrivaniaOperatorePortletKeys;
 import it.servizidigitali.scrivaniaoperatore.frontend.service.ScrivaniaOperatoreFrontendService;
 
-@Component(immediate = true, service = MVCActionCommand.class, property = {
-		"javax.portlet.name=" + ScrivaniaOperatorePortletKeys.SCRIVANIAOPERATORE,
-		"mvc.command.name=/action/prendiInCarico"
-})
+@Component(immediate = true, service = MVCActionCommand.class, property = { "javax.portlet.name=" + ScrivaniaOperatorePortletKeys.SCRIVANIAOPERATORE, "mvc.command.name=/action/prendiInCarico" })
 public class PrendiInCaricoActionCommand extends BaseMVCActionCommand {
 
 	@Reference
 	private ScrivaniaOperatoreFrontendService scrivaniaOperatoreFrontendService;
-	
+
 	@Override
 	protected void doProcessAction(ActionRequest request, ActionResponse response) throws Exception {
 		_log.debug("::doProcessAction");
-	
+
 		String taskId = ParamUtil.getString(request, "taskId");
-		
+
 		_log.debug("Task Id: " + taskId);
-		
-		ServiceContext context = ServiceContextFactory.getInstance(request);
-		scrivaniaOperatoreFrontendService.prendiTaskInCarico(String.valueOf(context.getUserId()), taskId);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+
+		scrivaniaOperatoreFrontendService.prendiTaskInCarico(themeDisplay.getUser().getScreenName(), taskId);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(PrendiInCaricoActionCommand.class);
