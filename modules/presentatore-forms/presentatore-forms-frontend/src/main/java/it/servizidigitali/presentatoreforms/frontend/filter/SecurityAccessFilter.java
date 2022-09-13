023bc21712774bd254ccf3a288d908cb26c44538
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-13 09:18:00 +0200

    Disabilitato security filter (da completare).

--- modules/presentatore-forms/presentatore-forms-frontend/src/main/java/it/servizidigitali/presentatoreforms/frontend/filter/SecurityAccessFilter.java
+++ modules/presentatore-forms/presentatore-forms-frontend/src/main/java/it/servizidigitali/presentatoreforms/frontend/filter/SecurityAccessFilter.java
@@ -2,15 +2,8 @@ package it.servizidigitali.presentatoreforms.frontend.filter;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
-import java.io.Serializable;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -23,7 +16,6 @@ import javax.portlet.filter.RenderFilter;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import it.servizidigitali.gestioneenti.model.ServizioEnte;
 import it.servizidigitali.gestioneenti.service.ServizioEnteLocalService;
 import it.servizidigitali.presentatoreforms.frontend.constants.PresentatoreFormsPortletKeys;
 
@@ -50,57 +42,63 @@ public class SecurityAccessFilter implements RenderFilter {
 	@Override
 	public void doFilter(RenderRequest request, RenderResponse response, FilterChain chain) throws IOException, PortletException {
 
-		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
-
-		Layout layout = themeDisplay.getLayout();
-
-		long groupId = layout.getGroupId();
-		long layoutId = layout.getLayoutId();
-
-		boolean isUserSignedIn = themeDisplay.isSignedIn();
-
-		ServizioEnte servizioEnte = servizioEnteLocalService.getServizioEnteByGroupIdLayoutId(groupId, layoutId);
-
-		if (servizioEnte == null) {
-			// TODO testare
-			log.debug("Servizio non trovato :: groupId: " + groupId + ", layoutId: " + layoutId);
-			throw new SecurityException("");
-		}
-		// TODO verifica servizio attivo
-		if (!servizioEnte.getAttivo()) {
-			log.debug("Servizio non attivo :: servizioId: " + servizioEnte.getServizioId() + ", organizationId: " + servizioEnte.getOrganizationId());
-			// TODO testare
-			throw new SecurityException("");
-		}
-
-		int livelloAutenticazione = servizioEnte.getLivelloAutenticazione();
-		boolean autenticazione = servizioEnte.getAutenticazione();
-		if (autenticazione && !isUserSignedIn) {
-			log.debug("Il servizio richiede autenticazione ma l'utente non è loggato :: servizioId: " + servizioEnte.getServizioId() + ", organizationId: " + servizioEnte.getOrganizationId());
-			// TODO testare
-			throw new SecurityException("");
-		}
-
-		// TODO verifica livello SPID utente coerente con quello del servizio
-		if (autenticazione && isUserSignedIn) {
-
-			PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
-			boolean omniadmin = permissionChecker.isOmniadmin();
-			if (!omniadmin) {
-				User loggedUser = themeDisplay.getUser();
-				Serializable livelloSpid = loggedUser.getExpandoBridge().getAttribute("livelloSPID");
-				if (livelloSpid != null) {
-					short livelloSpidUser = (short) livelloSpid;
-					if (livelloSpidUser < servizioEnte.getLivelloAutenticazione()) {
-						log.debug("Livello SPID non sufficiente per accedere al servizio :: servizioId: " + servizioEnte.getServizioId() + ", organizationId: " + servizioEnte.getOrganizationId()
-								+ ", livelloSpidUser: " + livelloSpidUser + ", livelloAutenticazione: " + servizioEnte.getLivelloAutenticazione());
-						// TODO implementare meccanismo per permettere il login con livello spid
-						// corretto (N.B.: DIPENDE DA IAM!)
-					}
-				}
-			}
-
-		}
+		// ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		//
+		// Layout layout = themeDisplay.getLayout();
+		//
+		// long groupId = layout.getGroupId();
+		// long layoutId = layout.getLayoutId();
+		//
+		// boolean isUserSignedIn = themeDisplay.isSignedIn();
+		//
+		// ServizioEnte servizioEnte =
+		// servizioEnteLocalService.getServizioEnteByGroupIdLayoutId(groupId, layoutId);
+		//
+		// if (servizioEnte == null) {
+		// // TODO testare
+		// log.debug("Servizio non trovato :: groupId: " + groupId + ", layoutId: " + layoutId);
+		// throw new SecurityException("");
+		// }
+		// // TODO verifica servizio attivo
+		// if (!servizioEnte.getAttivo()) {
+		// log.debug("Servizio non attivo :: servizioId: " + servizioEnte.getServizioId() + ",
+		// organizationId: " + servizioEnte.getOrganizationId());
+		// // TODO testare
+		// throw new SecurityException("");
+		// }
+		//
+		// int livelloAutenticazione = servizioEnte.getLivelloAutenticazione();
+		// boolean autenticazione = servizioEnte.getAutenticazione();
+		// if (autenticazione && !isUserSignedIn) {
+		// log.debug("Il servizio richiede autenticazione ma l'utente non è loggato :: servizioId: "
+		// + servizioEnte.getServizioId() + ", organizationId: " +
+		// servizioEnte.getOrganizationId());
+		// // TODO testare
+		// throw new SecurityException("");
+		// }
+		//
+		// // TODO verifica livello SPID utente coerente con quello del servizio
+		// if (autenticazione && isUserSignedIn) {
+		//
+		// PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
+		// boolean omniadmin = permissionChecker.isOmniadmin();
+		// if (!omniadmin) {
+		// User loggedUser = themeDisplay.getUser();
+		// Serializable livelloSpid = loggedUser.getExpandoBridge().getAttribute("livelloSPID");
+		// if (livelloSpid != null) {
+		// short livelloSpidUser = (short) livelloSpid;
+		// if (livelloSpidUser < servizioEnte.getLivelloAutenticazione()) {
+		// log.debug("Livello SPID non sufficiente per accedere al servizio :: servizioId: " +
+		// servizioEnte.getServizioId() + ", organizationId: " + servizioEnte.getOrganizationId()
+		// + ", livelloSpidUser: " + livelloSpidUser + ", livelloAutenticazione: " +
+		// servizioEnte.getLivelloAutenticazione());
+		// // TODO implementare meccanismo per permettere il login con livello spid
+		// // corretto (N.B.: DIPENDE DA IAM!)
+		// }
+		// }
+		// }
+		//
+		// }
 
 		chain.doFilter(request, response);
 	}
