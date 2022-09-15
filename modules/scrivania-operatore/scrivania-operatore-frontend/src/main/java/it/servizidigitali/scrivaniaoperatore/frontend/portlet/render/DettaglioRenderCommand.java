91812449680d4196d049f0fd06073beeeb46f25b
Michele Rizzitelli <michele.rizzitelli@linksmt.it>
2022-09-15 15:23:11 +0200

    Mostra azioni in base ai dati di camunda

--- modules/scrivania-operatore/scrivania-operatore-frontend/src/main/java/it/servizidigitali/scrivaniaoperatore/frontend/portlet/render/DettaglioRenderCommand.java
+++ modules/scrivania-operatore/scrivania-operatore-frontend/src/main/java/it/servizidigitali/scrivaniaoperatore/frontend/portlet/render/DettaglioRenderCommand.java
@@ -6,8 +6,10 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.ParamUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -20,6 +22,7 @@ import it.servizidigitali.camunda.integration.client.model.Task;
 import it.servizidigitali.scrivaniaoperatore.frontend.constants.ScrivaniaOperatorePortletKeys;
 import it.servizidigitali.scrivaniaoperatore.frontend.dto.AzioneUtente;
 import it.servizidigitali.scrivaniaoperatore.frontend.dto.RichiestaDTO;
+import it.servizidigitali.scrivaniaoperatore.frontend.enumeration.CamundaCodiciOperazioniUtente;
 import it.servizidigitali.scrivaniaoperatore.frontend.service.ScrivaniaOperatoreFrontendService;
 import it.servizidigitali.scrivaniaoperatore.frontend.util.MapUtil;
 import it.servizidigitali.scrivaniaoperatore.service.RichiestaLocalService;
@@ -59,14 +62,8 @@ public class DettaglioRenderCommand implements MVCRenderCommand {
 		Map<String, Task> userTasks = scrivaniaOperatoreFrontendService.getUserTasks(ctx);
 		boolean inCarico = userTasks.containsKey(richiesta.getProcessInstanceId());
 				
-		request.setAttribute("richiesta", richiesta);
-		request.setAttribute("inCarico", inCarico);
-		request.setAttribute("titleArgs", new String[] { String.valueOf(richiesta.getId()), richiesta.getServizio() });
-		
-		//TODO dimensione modello compilato dal cittadino
-		request.setAttribute("modelloArgs", "100 MB");
-		
 		List<AzioneUtente> azioni = scrivaniaOperatoreFrontendService.getAzioniUtenteDettaglioRichiesta(id, ctx);
+		Map<CamundaCodiciOperazioniUtente, Boolean> azioniMask = getAzioniMask(azioni);
 		
 		// TODO caricamento responsabili ed altri responsabili per modale
 		// List<User> responsabili =
@@ -75,7 +72,30 @@ public class DettaglioRenderCommand implements MVCRenderCommand {
 		// List<User> altriResponsabili =
 		// scrivaniaOperatoreFrontendService.getOrganizationUsersByRole(servizio.getSubOrganizationId(),
 		// OrganizationRole.ALTRO_RESPONSABILE, ctx.getCompanyId());
-
+		
+		request.setAttribute("hasAssegnaAltroResponsabile", azioniMask.get(CamundaCodiciOperazioniUtente.ASSEGNA_ALTRO_RESPONSABILE));
+		request.setAttribute("hasAssegnaResponsabile", azioniMask.get(CamundaCodiciOperazioniUtente.ASSEGNA_RESPONSABILE));
+		request.setAttribute("hasEsitoPositivo", azioniMask.get(CamundaCodiciOperazioniUtente.ESITO_PROCEDIMENTO_POSITIVO));
+		request.setAttribute("hasEsitoNegativo", azioniMask.get(CamundaCodiciOperazioniUtente.ESITO_PROCEDIMENTO_NEGATIVO));
+		request.setAttribute("hasRichiediModificheRichiedente", azioniMask.get(CamundaCodiciOperazioniUtente.RICHIESTA_INTEGRAZIONE));
+		request.setAttribute("hasRilascia", azioniMask.get(CamundaCodiciOperazioniUtente.RILASCIA_TASK));
+		
+		request.setAttribute("richiesta", richiesta);
+		request.setAttribute("inCarico", inCarico);
+		request.setAttribute("titleArgs", new String[] { String.valueOf(richiesta.getId()), richiesta.getServizio() });
+		//TODO dimensione modello compilato dal cittadino
+		request.setAttribute("modelloArgs", "100 MB");
+		
 		return "/dettaglio.jsp";
+	}
+	
+	private Map<CamundaCodiciOperazioniUtente, Boolean> getAzioniMask(List<AzioneUtente> azioni) {
+		List<CamundaCodiciOperazioniUtente> azioniKeys = azioni.stream()
+				.map(x -> x.getCodiceAzioneUtente())
+				.map(x -> CamundaCodiciOperazioniUtente.valueOf(x))
+				.collect(Collectors.toList());
+		
+		return Arrays.stream(CamundaCodiciOperazioniUtente.values())
+				.collect(Collectors.toMap(x -> x, x -> azioniKeys.contains(x)));
 	}
 }
--- modules/scrivania-operatore/scrivania-operatore-frontend/src/main/resources/META-INF/resources/dettaglio.jsp
+++ modules/scrivania-operatore/scrivania-operatore-frontend/src/main/resources/META-INF/resources/dettaglio.jsp
@@ -18,12 +18,41 @@
 
 <c:if test="${inCarico}">
 	<div class="row">
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="assegna-altro-responsabile" /></button>
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="assegna-responsabile" /></button>
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="chiudi-esito-positivo" /></button>
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="chiudi-esito-negativo" /></button>
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="richiedi-modifiche-integrazioni-richiedente" /></button>
-		<button class="btn btn-primary mr-1 mt-1"><liferay-ui:message key="rilascia" /></button>
+		<c:if test="${hasAssegnaAltroResponsabile}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="assegna-altro-responsabile" />
+			</button>
+		</c:if>
+		
+		<c:if test="${hasAssegnaResponsabile}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="assegna-responsabile" />
+			</button>
+		</c:if>
+				
+		<c:if test="${hasEsitoPositivo}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="chiudi-esito-positivo" />
+			</button>
+		</c:if>
+		
+		<c:if test="${hasEsitoNegativo}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="chiudi-esito-negativo" />
+			</button>
+		</c:if>
+		
+		<c:if test="${hasRichiediModificheRichiedente}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="richiedi-modifiche-integrazioni-richiedente" />
+			</button>
+		</c:if>
+		
+		<c:if test="${hasRilascia}">
+			<button class="btn btn-primary mr-1 mt-1">
+				<liferay-ui:message key="rilascia" />
+			</button>
+		</c:if>
 	</div>
 </c:if>
 
