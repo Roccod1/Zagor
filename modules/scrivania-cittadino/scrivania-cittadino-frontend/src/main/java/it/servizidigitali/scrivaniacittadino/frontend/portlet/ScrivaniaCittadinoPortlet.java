c4b1be29b9bf1e8d6db9acee0ca8dcd9c2d706a3
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-15 11:47:19 +0200

    Eliminate variabili di classe non utilizzate.

--- modules/scrivania-cittadino/scrivania-cittadino-frontend/src/main/java/it/servizidigitali/scrivaniacittadino/frontend/portlet/ScrivaniaCittadinoPortlet.java
+++ modules/scrivania-cittadino/scrivania-cittadino-frontend/src/main/java/it/servizidigitali/scrivaniacittadino/frontend/portlet/ScrivaniaCittadinoPortlet.java
@@ -3,11 +3,7 @@ package it.servizidigitali.scrivaniacittadino.frontend.portlet;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,59 +29,52 @@ import it.servizidigitali.scrivaniacittadino.frontend.constants.ScrivaniaCittadi
 /**
  * @author pindi
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.servizidigitali",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-//		"com.liferay.portlet.header-portlet-js=/js/main.js",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=ScrivaniaCittadino",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + ScrivaniaCittadinoPortletKeys.SCRIVANIACITTADINO,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+@Component(//
+		immediate = true, //
+		property = { //
+				"com.liferay.portlet.display-category=category.servizidigitali", //
+				"com.liferay.portlet.header-portlet-css=/css/main.css", //
+				"com.liferay.portlet.instanceable=true", //
+				"javax.portlet.display-name=ScrivaniaCittadino", //
+				"javax.portlet.init-param.template-path=/", //
+				"javax.portlet.init-param.view-template=/view.jsp", //
+				"javax.portlet.name=" + ScrivaniaCittadinoPortletKeys.SCRIVANIACITTADINO, //
+				"javax.portlet.resource-bundle=content.Language", //
+				"javax.portlet.security-role-ref=power-user,user"//
+		}, //
+		service = Portlet.class//
+) //
 public class ScrivaniaCittadinoPortlet extends MVCPortlet {
-	
+
 	private static final Log _log = LogFactoryUtil.getLog(ScrivaniaCittadinoPortlet.class);
-	
+
 	@Reference
 	private ComunicazioneLocalService comunicazioneLocalService;
-	
+
 	@Reference
 	private TipologiaComunicazioneLocalService tipologiaComunicazioneLocalService;
-	
+
 	@Reference
 	private TipologiaLocalService tipologiaLocalService;
-	
+
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-		
-       ServiceContext serviceContext = null;
-       ThemeDisplay themeDisplay = null;
-       List<TipologiaComunicazione> listaTipologiaComunicazione = new ArrayList<TipologiaComunicazione>();
-       List<Tipologia> listaTipologiaServizi = new ArrayList<Tipologia>();
-       try {
-            serviceContext = ServiceContextFactory.getInstance(renderRequest);
-            themeDisplay = serviceContext.getThemeDisplay();
-            User loggedUser = themeDisplay.getUser();
 
-            listaTipologiaComunicazione = tipologiaComunicazioneLocalService.getTipologiaComunicaziones(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-            listaTipologiaServizi = tipologiaLocalService.getTipologias(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-		}catch (Exception e) {
-		    _log.error("render() :: " + e.getMessage(), e);
+		List<TipologiaComunicazione> listaTipologiaComunicazione = new ArrayList<TipologiaComunicazione>();
+		List<Tipologia> listaTipologiaServizi = new ArrayList<Tipologia>();
+		try {
+			listaTipologiaComunicazione = tipologiaComunicazioneLocalService.getTipologiaComunicaziones(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+			listaTipologiaServizi = tipologiaLocalService.getTipologias(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 		}
-       
-   		renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_TIPOLOGIA_COMUNICAZIONE, listaTipologiaComunicazione);
-       	renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_STATO, Arrays.asList(StatoRichiesta.values()));
-       	renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_TIPOLOGIA_RICHIESTA, listaTipologiaServizi);
+		catch (Exception e) {
+			_log.error("render :: " + e.getMessage(), e);
+		}
 
-   		super.render(renderRequest, renderResponse);
+		renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_TIPOLOGIA_COMUNICAZIONE, listaTipologiaComunicazione);
+		renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_STATO, Arrays.asList(StatoRichiesta.values()));
+		renderRequest.setAttribute(ScrivaniaCittadinoPortletKeys.LISTA_TIPOLOGIA_RICHIESTA, listaTipologiaServizi);
+
+		super.render(renderRequest, renderResponse);
 	}
-	
 
 }
