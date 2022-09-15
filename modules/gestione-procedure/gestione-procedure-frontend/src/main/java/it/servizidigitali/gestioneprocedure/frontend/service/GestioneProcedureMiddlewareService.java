144de12aaaa791e9bd99b175b82e4b99d6e043e5
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-15 18:20:34 +0200

    Aggiunta eliminazione templates jrxml se tipo generazione NATIVA.

--- modules/gestione-procedure/gestione-procedure-frontend/src/main/java/it/servizidigitali/gestioneprocedure/frontend/service/GestioneProcedureMiddlewareService.java
+++ modules/gestione-procedure/gestione-procedure-frontend/src/main/java/it/servizidigitali/gestioneprocedure/frontend/service/GestioneProcedureMiddlewareService.java
@@ -20,7 +20,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import java.util.stream.Collectors;
 
 import org.osgi.service.component.annotations.Component;
@@ -59,24 +58,23 @@ public class GestioneProcedureMiddlewareService {
 
 	@Reference
 	private ProceduraFormLocalService proceduraFormLocalService;
-	
+
 	@Reference
 	private TemplatePdfLocalService templatePdfLocalService;
-	
+
 	@Reference
 	private CounterLocalService counterLocalService;
-	
+
 	@Reference
 	private FileService fileService;
-	
+
 	@Reference
 	private DLAppService dlAppService;
 
 	public Procedura getProcedura(long groupId, long servizioId, boolean attiva) {
 		Procedura procedura = null;
 
-		List<Procedura> listaProcedure = proceduraLocalService.getProcedureByGroupIdServizioIdAttiva(groupId,
-				servizioId, attiva);
+		List<Procedura> listaProcedure = proceduraLocalService.getProcedureByGroupIdServizioIdAttiva(groupId, servizioId, attiva);
 
 		if (Validator.isNotNull(listaProcedure) && !listaProcedure.isEmpty()) {
 			for (Procedura p : listaProcedure) {
@@ -92,8 +90,7 @@ public class GestioneProcedureMiddlewareService {
 
 	public List<Servizio> getServiziByOrganizationAttivo(long organizationId) throws Exception {
 
-		List<Long> listaServiziEnte = servizioEnteLocalService.getListaServiziByOrganizationAttivo(organizationId,
-				true);
+		List<Long> listaServiziEnte = servizioEnteLocalService.getListaServiziByOrganizationAttivo(organizationId, true);
 		List<Servizio> listaServizi = new ArrayList<Servizio>();
 
 		if (Validator.isNotNull(listaServiziEnte)) {
@@ -102,11 +99,13 @@ public class GestioneProcedureMiddlewareService {
 
 				if (Validator.isNotNull(servizio)) {
 					listaServizi.add(servizio);
-				} else {
+				}
+				else {
 					_log.error("Impossibile recuperare il servizio per ID: " + idServizio);
 				}
 			}
-		} else {
+		}
+		else {
 			_log.error("Impossibile recuperare la lista dei servizi per questa organizzazione : " + organizationId);
 		}
 
@@ -142,15 +141,18 @@ public class GestioneProcedureMiddlewareService {
 				if (!listaFormPrincipali.isEmpty()) {
 					if (listaFormPrincipali.size() == 1) {
 						formPrincipale = listaFormPrincipali.get(0);
-					} else {
+					}
+					else {
 						_log.error("Trovato piu' di un form principale per la procedura con ID : " + proceduraId);
 					}
-				} else {
+				}
+				else {
 					_log.error("Non e' stato trovato nessun form principale per la procedura con ID : " + proceduraId);
 				}
 			}
 
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			_log.error("Errore durante il recupero del form principale : " + e.getMessage());
 		}
 
@@ -176,7 +178,6 @@ public class GestioneProcedureMiddlewareService {
 						}
 					}
 				}
-
 			}
 		}
 
@@ -207,10 +208,10 @@ public class GestioneProcedureMiddlewareService {
 		if (Validator.isNotNull(listaProceduraFormRecuperatiDb) && !listaProceduraFormRecuperatiDb.isEmpty()) {
 
 			if (Validator.isNotNull(listaProceduraFormRecuperatiForm) && Validator.isNotNull(formPrincipale)) {
-				List<ProceduraForm> elementiDaEliminare = listaProceduraFormRecuperatiDb.stream().filter(proceduraForm -> !listaProceduraFormRecuperatiForm.contains(proceduraForm)
-								&& proceduraForm.getFormId() != formPrincipale.getFormId()).collect(Collectors.toList());
-				List<ProceduraForm> elementiDaAggiungere = listaProceduraFormRecuperatiForm.stream().filter(proceduraForm -> !listaProceduraFormRecuperatiDb.contains(proceduraForm)
-								&& proceduraForm.getFormId() != formPrincipale.getFormId()).collect(Collectors.toList());
+				List<ProceduraForm> elementiDaEliminare = listaProceduraFormRecuperatiDb.stream()
+						.filter(proceduraForm -> !listaProceduraFormRecuperatiForm.contains(proceduraForm) && proceduraForm.getFormId() != formPrincipale.getFormId()).collect(Collectors.toList());
+				List<ProceduraForm> elementiDaAggiungere = listaProceduraFormRecuperatiForm.stream()
+						.filter(proceduraForm -> !listaProceduraFormRecuperatiDb.contains(proceduraForm) && proceduraForm.getFormId() != formPrincipale.getFormId()).collect(Collectors.toList());
 
 				for (ProceduraForm proceduraForm : elementiDaEliminare) {
 					proceduraFormLocalService.deleteProceduraForm(proceduraForm);
@@ -221,7 +222,8 @@ public class GestioneProcedureMiddlewareService {
 				}
 
 			}
-		} else {
+		}
+		else {
 			for (ProceduraForm proceduraForm : listaProceduraFormRecuperatiForm) {
 				proceduraFormLocalService.updateProceduraForm(proceduraForm);
 			}
@@ -260,255 +262,273 @@ public class GestioneProcedureMiddlewareService {
 	}
 
 	public List<Servizio> getListaServiziAssociabiliProcedura(long organizationId, long groupId, long proceduraId) {
-		
+
 		_log.debug("Recupero elenco servizi associabili a procedura per groupId: " + groupId);
-		
+
 		List<Servizio> listaServiziAssociabili = new ArrayList<>();
 
 		try {
-			//lista di tutti i servizi ente 
-			List<Long> listaServizioEnteId =  servizioEnteLocalService.getListaServiziByOrganizationAttivo(organizationId, true);
-			
-			//lista dei servizi per specifico groupId che gia' associati ad una procedura
+			// lista di tutti i servizi ente
+			List<Long> listaServizioEnteId = servizioEnteLocalService.getListaServiziByOrganizationAttivo(organizationId, true);
+
+			// lista dei servizi per specifico groupId che gia' associati ad una procedura
 			List<Procedura> listaProcedure = proceduraLocalService.getProcedureByGroupIdAttiva(groupId, true);
 			List<Long> listaServiziAssociatiId = listaProcedure.stream().filter(p -> p.getProceduraId() != proceduraId).map(p -> p.getServizioId()).collect(Collectors.toList());
-			
-			//lista servizi assegnabili ad una procedura
+
+			// lista servizi assegnabili ad una procedura
 			List<Long> listaServiziAssegnabiliId = listaServizioEnteId.stream().filter(idServizio -> !listaServiziAssociatiId.contains(idServizio)).collect(Collectors.toList());
-			
-			if(Validator.isNotNull(listaServiziAssegnabiliId)) {
-				for(Long servizioId : listaServiziAssegnabiliId) {
+
+			if (Validator.isNotNull(listaServiziAssegnabiliId)) {
+				for (Long servizioId : listaServiziAssegnabiliId) {
 					Servizio servizio = servizioLocalService.getServizio(servizioId);
-					listaServiziAssociabili.add(servizio);			
+					listaServiziAssociabili.add(servizio);
 				}
 			}
-			
-		} catch (Exception e) {
+
+		}
+		catch (Exception e) {
 			_log.error("Errore durante il recupero dell'elenco servizi associabili a procedura: " + e);
 		}
-		
+
 		return listaServiziAssociabili;
 	}
-	
-	public TemplatePdf caricaTemplatePdf(File file, String nomeFile, boolean principale, long proceduraId, long templatePdfId, int indice, String userFullName, long userId, long groupId, long companyId) {
 
-        InputStream fileCaricato = null;
-        
-        String nomeFileNuovo = "";
-        long fileEntryId = 0;
-        
-        if(Validator.isNotNull(file)) {
-            nomeFile = nomeFile + "_" + String.valueOf(proceduraId) + nowToString();
-        }
-		
+	public TemplatePdf caricaTemplatePdf(File file, String nomeFile, boolean principale, long proceduraId, long templatePdfId, int indice, String userFullName, long userId, long groupId,
+			long companyId) {
+
+		InputStream fileCaricato = null;
+
+		String nomeFileNuovo = "";
+		long fileEntryId = 0;
+
+		if (Validator.isNotNull(file)) {
+			nomeFile = nomeFile + "_" + String.valueOf(proceduraId) + nowToString();
+		}
+
 		try {
-			if(templatePdfId>0 && Validator.isNotNull(file)) {
+			if (templatePdfId > 0 && Validator.isNotNull(file)) {
 				byte[] fileAggiornato = FileUtil.getBytes(file);
 				FileEntry fileRecuperato = getFileEntryTemplatePdf(templatePdfId);
 				fileEntryId = fileService.updateFileEntry(fileRecuperato, fileAggiornato, nomeFile, groupId, userId);
-			}else {
-             	fileCaricato = new DataInputStream(new FileInputStream(file));
-             	fileEntryId = fileService.saveJasperReport(nomeFile, nomeFile, nomeFile, proceduraId, fileCaricato, null, userId, groupId);
 			}
-		}catch(Exception e) {
+			else {
+				fileCaricato = new DataInputStream(new FileInputStream(file));
+				fileEntryId = fileService.saveJasperReport(nomeFile, nomeFile, nomeFile, proceduraId, fileCaricato, null, userId, groupId);
+			}
+		}
+		catch (Exception e) {
 			_log.error("Errore durante l'aggiornamento del file del template con ID : " + templatePdfId);
 		}
 
 		TemplatePdf templatePdf = aggiornaCreaTemplatePdf(templatePdfId, proceduraId, principale, userId, companyId, userFullName, fileEntryId);
-		
+
 		return templatePdf;
 	}
-	
+
 	public TemplatePdf aggiornaCreaTemplatePdf(long templatePdfId, long proceduraId, boolean principale, long userId, long companyId, String userFullName, long fileEntryId) {
 		TemplatePdf templatePdf = null;
 		long idTemplatePdfPrincipale = 0;
-		
+
 		try {
-			if(templatePdfId>0) {
+			if (templatePdfId > 0) {
 				templatePdf = templatePdfLocalService.getTemplatePdf(templatePdfId);
-			}else {
+			}
+			else {
 				templatePdf = templatePdfLocalService.createTemplatePdf(counterLocalService.increment());
 			}
-			
-			
-            templatePdf.setUserId(userId);
-            templatePdf.setCompanyId(companyId);
-            templatePdf.setUserName(userFullName);
-            
-            if(fileEntryId>0) {
-         		templatePdf.setFileEntryId(fileEntryId);
-         	}
-            
-            templatePdf.setProceduraId(proceduraId);
-            templatePdf.setAttivo(Boolean.TRUE);
-            
-            if(principale && idTemplatePdfPrincipale==0) {
-              	 idTemplatePdfPrincipale = templatePdf.getTemplatePdfId();
-            }
-               
-            if(!principale && idTemplatePdfPrincipale>0) {
-             	 templatePdf.setTemplatePdfParentId(idTemplatePdfPrincipale);
-            }
-            
-            if(!principale && idTemplatePdfPrincipale==0) {
-            	TemplatePdf templatePrincipale = recuperaTemplatePdfPrincipale(proceduraId);
-            	
-            	if(Validator.isNotNull(templatePrincipale)) {
-            		templatePdf.setTemplatePdfParentId(templatePrincipale.getTemplatePdfId());
-            	}
-            }
-               
-            templatePdfLocalService.updateTemplatePdf(templatePdf);
-            return templatePdf;
 
-		}catch(Exception e) {
+			templatePdf.setUserId(userId);
+			templatePdf.setCompanyId(companyId);
+			templatePdf.setUserName(userFullName);
+
+			if (fileEntryId > 0) {
+				templatePdf.setFileEntryId(fileEntryId);
+			}
+
+			templatePdf.setProceduraId(proceduraId);
+			templatePdf.setAttivo(Boolean.TRUE);
+
+			if (principale && idTemplatePdfPrincipale == 0) {
+				idTemplatePdfPrincipale = templatePdf.getTemplatePdfId();
+			}
+
+			if (!principale && idTemplatePdfPrincipale > 0) {
+				templatePdf.setTemplatePdfParentId(idTemplatePdfPrincipale);
+			}
+
+			if (!principale && idTemplatePdfPrincipale == 0) {
+				TemplatePdf templatePrincipale = recuperaTemplatePdfPrincipale(proceduraId);
+
+				if (Validator.isNotNull(templatePrincipale)) {
+					templatePdf.setTemplatePdfParentId(templatePrincipale.getTemplatePdfId());
+				}
+			}
+
+			templatePdfLocalService.updateTemplatePdf(templatePdf);
+			return templatePdf;
+
+		}
+		catch (Exception e) {
 			_log.error("Errore durante il salvataggio/aggiornamento del templatePdf! : " + e.getMessage());
 		}
-		
+
 		return templatePdf;
-		
+
 	}
-	
+
 	public void aggiornaPrincipaleTemplatePdf(TemplatePdf templatePrincipale, long templatePdfId) {
 
-		List<TemplatePdf> listaTemplateProcedura = recuperaTemplatePdfProceduraAttivo(templatePrincipale.getProceduraId(),true);
-		
+		List<TemplatePdf> listaTemplateProcedura = recuperaTemplatePdfProceduraAttivo(templatePrincipale.getProceduraId(), true);
+
 		try {
-			
-			if(Validator.isNotNull(listaTemplateProcedura) && !listaTemplateProcedura.isEmpty()) {
-								
+
+			if (Validator.isNotNull(listaTemplateProcedura) && !listaTemplateProcedura.isEmpty()) {
+
 				// Aggiorno tutti i figli
-				
-				for(TemplatePdf templatePdf : listaTemplateProcedura) {
-					if(templatePdf.getTemplatePdfParentId()==templatePrincipale.getTemplatePdfId()) {
+
+				for (TemplatePdf templatePdf : listaTemplateProcedura) {
+					if (templatePdf.getTemplatePdfParentId() == templatePrincipale.getTemplatePdfId()) {
 						templatePdf.setTemplatePdfParentId(templatePdfId);
 						templatePdfLocalService.updateTemplatePdf(templatePdf);
 					}
 				}
-				
+
 				templatePrincipale.setTemplatePdfParentId(templatePdfId);
 				templatePdfLocalService.updateTemplatePdf(templatePrincipale);
-				
+
 				// Aggiorno il nuovo principale
-				
+
 				TemplatePdf templatePdfNuovoPrincipale = templatePdfLocalService.getTemplatePdf(templatePdfId);
-				
-				if(Validator.isNotNull(templatePdfNuovoPrincipale)) {
+
+				if (Validator.isNotNull(templatePdfNuovoPrincipale)) {
 					templatePdfNuovoPrincipale.setTemplatePdfParentId(0);
 					templatePdfLocalService.updateTemplatePdf(templatePdfNuovoPrincipale);
 				}
-			}		
+			}
 
-		}catch(Exception e) {
+		}
+		catch (Exception e) {
 			_log.error("Errore durante l'aggiornamento del report principale!");
 		}
 
 	}
-	
-	
-	public List<TemplatePdf> recuperaTemplatePdfProceduraAttivo(long proceduraId, boolean attivo){
+
+	public List<TemplatePdf> recuperaTemplatePdfProceduraAttivo(long proceduraId, boolean attivo) {
 		List<TemplatePdf> listaTemplatePdf = null;
 		List<TemplatePdf> listaTemplatePdfNomeFile = null;
-		
-		if(proceduraId>0) {
+
+		if (proceduraId > 0) {
 			listaTemplatePdf = new ArrayList<TemplatePdf>();
 			listaTemplatePdfNomeFile = new ArrayList<TemplatePdf>();
 			listaTemplatePdf = templatePdfLocalService.findByProceduraIdAndAttivo(proceduraId, attivo);
-			
+
 			try {
-				if(Validator.isNotNull(listaTemplatePdf) && !listaTemplatePdf.isEmpty()) {
-					for(TemplatePdf pdf : listaTemplatePdf) {
-						
-						if(Validator.isNotNull(pdf)) {
+				if (Validator.isNotNull(listaTemplatePdf) && !listaTemplatePdf.isEmpty()) {
+					for (TemplatePdf pdf : listaTemplatePdf) {
+
+						if (Validator.isNotNull(pdf)) {
 							FileEntry file = dlAppService.getFileEntry(pdf.getFileEntryId());
-							
-							if(Validator.isNotNull(file)) {
+
+							if (Validator.isNotNull(file)) {
 								pdf.setNomeFile(file.getFileName());
 							}
 							listaTemplatePdfNomeFile.add(pdf);
 						}
 					}
 				}
-			} catch (Exception e) {
+			}
+			catch (Exception e) {
 				_log.error("Errore durante il recupero del file da sistema!" + e.getMessage());
 			}
-			
-			
+
 		}
-		
+
 		return listaTemplatePdfNomeFile;
 	}
-	
+
 	public FileEntry getFileEntryTemplatePdf(long templatePdfId) {
 		FileEntry file = null;
 		TemplatePdf templatePdf = null;
-		
+
 		try {
-			if(templatePdfId>0) {
+			if (templatePdfId > 0) {
 				templatePdf = templatePdfLocalService.getTemplatePdf(templatePdfId);
-				if(Validator.isNotNull(templatePdf)) {
+				if (Validator.isNotNull(templatePdf)) {
 					file = dlAppService.getFileEntry(templatePdf.getFileEntryId());
 				}
 			}
-		}catch(Exception e) {
+		}
+		catch (Exception e) {
 			_log.error("Errore durante il recupero del file: " + e.getMessage());
 		}
-		
+
 		return file;
 	}
-	
-	public void cancellaTemplatePdf(List<Long> listaReportDaMantenere, long proceduraId) {
+
+	public void deleteTemplatePdf(List<Long> listaReportDaMantenere, long proceduraId) {
 		List<TemplatePdf> listaTemplatePdf = null;
-		if(proceduraId>0) {
+		if (proceduraId > 0) {
 			listaTemplatePdf = new ArrayList<TemplatePdf>();
 			listaTemplatePdf = templatePdfLocalService.findByProceduraId(proceduraId);
 		}
-				
+
 		try {
-			for(TemplatePdf template : listaTemplatePdf) {
-				if(!listaReportDaMantenere.contains(template.getTemplatePdfId())) {
-					if(template.getTemplatePdfParentId()>0) {
+			for (TemplatePdf template : listaTemplatePdf) {
+				if (!listaReportDaMantenere.contains(template.getTemplatePdfId())) {
+					if (template.getTemplatePdfParentId() > 0) {
 						template.setAttivo(false);
 						templatePdfLocalService.updateTemplatePdf(template);
-					}else {
+					}
+					else {
 						_log.error("Impossibile eliminare l'allegato jasper con ID : " + template.getTemplatePdfId() + "perché è principale");
 					}
 				}
 			}
-		}catch(Exception e) {
+		}
+		catch (Exception e) {
 			_log.error("Errore durante la cancellazione del template: " + e.getMessage());
 		}
 
 	}
-	
-	
-	public TemplatePdf recuperaTemplatePdfPrincipale (long proceduraId) {
+
+	public void deleteTemplatePdfProcedura(long proceduraId) {
+		List<TemplatePdf> findByProceduraId = templatePdfLocalService.findByProceduraId(proceduraId);
+		if (findByProceduraId != null) {
+			for (TemplatePdf templatePdf : findByProceduraId) {
+				try {
+					fileService.deleteRequestFile(templatePdf.getFileEntryId());
+					templatePdfLocalService.deleteTemplatePdf(templatePdf.getTemplatePdfId());
+				}
+				catch (PortalException e) {
+					_log.error("deleteTemplatePdfProcedura :: " + e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	public TemplatePdf recuperaTemplatePdfPrincipale(long proceduraId) {
 		TemplatePdf template = null;
 		List<TemplatePdf> listaTemplatePdf = null;
-		
-		if(proceduraId>0) {
+
+		if (proceduraId > 0) {
 			listaTemplatePdf = new ArrayList<TemplatePdf>();
 			listaTemplatePdf = templatePdfLocalService.findByProceduraId(proceduraId);
 		}
-		
-		if(Validator.isNotNull(listaTemplatePdf) && !listaTemplatePdf.isEmpty()) {
-			for(TemplatePdf templatePdf : listaTemplatePdf) {
-				if(templatePdf.getTemplatePdfParentId()==0) {
+
+		if (Validator.isNotNull(listaTemplatePdf) && !listaTemplatePdf.isEmpty()) {
+			for (TemplatePdf templatePdf : listaTemplatePdf) {
+				if (templatePdf.getTemplatePdfParentId() == 0) {
 					template = templatePdf;
 				}
 			}
 		}
-		
+
 		return template;
 	}
-	
-	
 
-	
 	public static String nowToString() {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
 		return sdf.format(new Date());
 	}
-	
-	
+
 }
