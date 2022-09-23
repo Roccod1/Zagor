b89f13e2ffc2f186722ee389d480b1878382bb28
Luigi <luigi.pasca@gmail.com>
2022-09-23 10:04:07 +0200

    Generazione token per screenname

--- modules/servizi-digitali-rest-service/servizi-digitali-rest-service-jwt-utility/src/main/java/it/servizidigitali/restservice/jwt/utility/api/JwtUtilityService.java
+++ modules/servizi-digitali-rest-service/servizi-digitali-rest-service-jwt-utility/src/main/java/it/servizidigitali/restservice/jwt/utility/api/JwtUtilityService.java
@@ -7,8 +7,10 @@ import com.nimbusds.jwt.JWT;
 import it.servizidigitali.restservice.jwt.utility.exception.JwtException;
 
 public interface JwtUtilityService {
-
+	
 	public String getJWTUserToken(HttpServletRequest request); 
+
+	public String getJWTUserToken(Long companyId, String screenName); 
 	
 	public JWT verifyJwt(String token) throws JwtException;
 	
