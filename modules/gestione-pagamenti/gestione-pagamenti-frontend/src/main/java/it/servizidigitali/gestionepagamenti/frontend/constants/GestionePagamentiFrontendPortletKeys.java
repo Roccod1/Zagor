27e804dbcad623e5b951bbf0d7c76ee87875c13b
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-09-26 10:47:26 +0200

    Aggiunto scheletro portlet Gestione Pagamenti.

---
+++ modules/gestione-pagamenti/gestione-pagamenti-frontend/src/main/java/it/servizidigitali/gestionepagamenti/frontend/constants/GestionePagamentiFrontendPortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.gestionepagamenti.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class GestionePagamentiFrontendPortletKeys {
+
+	public static final String GESTIONEPAGAMENTIFRONTEND =
+		"gestione_pagamenti_frontend_GestionePagamentiFrontendPortlet";
+
+}
---
+++ modules/gestione-pagamenti/gestione-pagamenti-frontend/src/main/java/it/servizidigitali/gestionepagamenti/frontend/portlet/GestionePagamentiFrontendPortlet.java
@@ -0,0 +1,30 @@
+package it.servizidigitali.gestionepagamenti.frontend.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+import it.servizidigitali.gestionepagamenti.frontend.constants.GestionePagamentiFrontendPortletKeys;
+
+/**
+ * @author pindi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.servizidigitali",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=GestionePagamentiFrontend",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GestionePagamentiFrontendPortletKeys.GESTIONEPAGAMENTIFRONTEND,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GestionePagamentiFrontendPortlet extends MVCPortlet {
+}
