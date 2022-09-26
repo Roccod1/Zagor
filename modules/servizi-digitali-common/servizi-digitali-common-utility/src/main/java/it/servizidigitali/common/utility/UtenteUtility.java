a9c4295ab5b3d225bd9ff553f52e0b0b641e1708
Michele Rizzitelli <michele.rizzitelli@linksmt.it>
2022-09-26 13:50:12 +0200

    Fix indirizzo residenza

--- modules/servizi-digitali-common/servizi-digitali-common-utility/src/main/java/it/servizidigitali/common/utility/UtenteUtility.java
+++ modules/servizi-digitali-common/servizi-digitali-common-utility/src/main/java/it/servizidigitali/common/utility/UtenteUtility.java
@@ -1,15 +1,13 @@
 package it.servizidigitali.common.utility;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
-
-import java.io.Serializable;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import it.servizidigitali.common.utility.enumeration.UserCustomAttributes;
 import it.servizidigitali.common.utility.exception.CommonUtilityException;
@@ -36,36 +34,41 @@ public class UtenteUtility {
 	 * @throws CommonUtilityException
 	 */
 	public IndirizzoResidenza getIndirizzoRedidenza(long companyId, String screenName) throws CommonUtilityException {
-
 		try {
+			IndirizzoResidenza indirizzoResidenza;
+			
 			User userByScreenName = userLocalService.getUserByScreenName(companyId, screenName);
-			if (userByScreenName != null) {
-				Serializable residenza = userByScreenName.getExpandoBridge().getAttribute(UserCustomAttributes.RESIDENZA.getNomeAttributo());
-				IndirizzoResidenza indirizzoResidenza = new IndirizzoResidenza();
-				if (residenza != null) {
-					String residenzaString = (String) residenza;
-					String[] split = residenzaString.split(StringPool.SPACE);
-					
-					if (split.length >= 5) {
-						indirizzoResidenza.setTipologia(split[0]);
-						indirizzoResidenza.setIndirizzo(split[1]);
-						indirizzoResidenza.setCivico(split[2]);
-						indirizzoResidenza.setCap(split[3]);
-						indirizzoResidenza.setLuogo(split[4]);
-						if (split.length > 5) {
-							indirizzoResidenza.setProvincia(split[5]);
-						}
-					} else {
-						return null;
-					}
-				}
-				return indirizzoResidenza;
+			if (userByScreenName == null) {
+				throw new CommonUtilityException("getUserByScreenName 0 results");
 			}
-		}
-		catch (Exception e) {
+			
+			String residenza = (String) userByScreenName.getExpandoBridge().getAttribute(UserCustomAttributes.RESIDENZA.getNomeAttributo());
+			
+			if (residenza != null) {
+				String[] split = residenza.split(StringPool.SPACE);
+				
+				//TODO formato della residenza?
+				if (split.length >= 5) {
+					indirizzoResidenza = new IndirizzoResidenza();
+					indirizzoResidenza.setTipologia(split[0]);
+					indirizzoResidenza.setIndirizzo(split[1]);
+					indirizzoResidenza.setCivico(split[2]);
+					indirizzoResidenza.setCap(split[3]);
+					indirizzoResidenza.setLuogo(split[4]);
+					if (split.length > 5) {
+						indirizzoResidenza.setProvincia(split[5]);
+					}
+				} else {
+					throw new CommonUtilityException("residenza str malformed");
+				}
+			} else {
+				indirizzoResidenza = null;
+			}
+			
+			return indirizzoResidenza;
+		} catch (Exception e) {
 			log.error("getIndirizzoRedidenzaMap :: " + e.getMessage(), e);
 			throw new CommonUtilityException(e);
 		}
-		return null;
 	}
 }
