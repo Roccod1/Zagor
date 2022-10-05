2893ccda77c38e1b455f6cc295606ed0f9ddf603
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-10-05 13:19:46 +0200

    Implementato servizio rest getRichiestaServizio.

--- modules/servizi-digitali-rest-service/servizi-digitali-rest-service-client/src/main/java/it/servizidigitali/restservice/client/serdes/v1_0/RichiestaServizioSerDes.java
+++ modules/servizi-digitali-rest-service/servizi-digitali-rest-service-client/src/main/java/it/servizidigitali/restservice/client/serdes/v1_0/RichiestaServizioSerDes.java
@@ -3,6 +3,9 @@ package it.servizidigitali.restservice.client.serdes.v1_0;
 import it.servizidigitali.restservice.client.dto.v1_0.RichiestaServizio;
 import it.servizidigitali.restservice.client.json.BaseJSONParser;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
@@ -41,6 +44,201 @@ public class RichiestaServizioSerDes {
 
 		sb.append("{");
 
+		DateFormat liferayToJSONDateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+		if (richiestaServizio.getCodiceAreaTematica() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"codiceAreaTematica\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getCodiceAreaTematica()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getCodiceFiscaleDelegato() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"codiceFiscaleDelegato\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getCodiceFiscaleDelegato()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getCodiceFiscaleRichiedente() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"codiceFiscaleRichiedente\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getCodiceFiscaleRichiedente()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getCodiceIdentificativoServizio() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"codiceIdentificativoServizio\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				_escape(richiestaServizio.getCodiceIdentificativoServizio()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDataAggiornamento() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"dataAggiornamento\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataAggiornamento()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDataInserimento() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"dataInserimento\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataInserimento()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDataProtocollo() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"dataProtocollo\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataProtocollo()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDataProtocolloEsterno() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"dataProtocolloEsterno\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataProtocolloEsterno()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDenominazioneAreaTematica() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"denominazioneAreaTematica\": ");
+
+			sb.append("\"");
+
+			sb.append(
+				_escape(richiestaServizio.getDenominazioneAreaTematica()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDenominazioneEnte() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"denominazioneEnte\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getDenominazioneEnte()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDenominazioneRichiedente() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"denominazioneRichiedente\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getDenominazioneRichiedente()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getDenominazioneServizio() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"denominazioneServizio\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getDenominazioneServizio()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getEmailRichiedente() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"emailRichiedente\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getEmailRichiedente()));
+
+			sb.append("\"");
+		}
+
 		if (richiestaServizio.getId() != null) {
 			if (sb.length() > 1) {
 				sb.append(", ");
@@ -49,6 +247,114 @@ public class RichiestaServizioSerDes {
 			sb.append("\"id\": ");
 
 			sb.append(richiestaServizio.getId());
+		}
+
+		if (richiestaServizio.getIdProcedura() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"idProcedura\": ");
+
+			sb.append(richiestaServizio.getIdProcedura());
+		}
+
+		if (richiestaServizio.getIdentificativoEnte() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"identificativoEnte\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getIdentificativoEnte()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getIdentificativoProcesso() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"identificativoProcesso\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getIdentificativoProcesso()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getNote() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"note\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getNote()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getNumeroProtocollo() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"numeroProtocollo\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getNumeroProtocollo()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getNumeroProtocolloEsterno() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"numeroProtocolloEsterno\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getNumeroProtocolloEsterno()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getOggetto() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"oggetto\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getOggetto()));
+
+			sb.append("\"");
+		}
+
+		if (richiestaServizio.getPartitaIvaRichiedente() != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"partitaIvaRichiedente\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(richiestaServizio.getPartitaIvaRichiedente()));
+
+			sb.append("\"");
 		}
 
 		if (richiestaServizio.getStato() != null) {
@@ -86,11 +392,207 @@ public class RichiestaServizioSerDes {
 
 		Map<String, String> map = new TreeMap<>();
 
+		DateFormat liferayToJSONDateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+		if (richiestaServizio.getCodiceAreaTematica() == null) {
+			map.put("codiceAreaTematica", null);
+		}
+		else {
+			map.put(
+				"codiceAreaTematica",
+				String.valueOf(richiestaServizio.getCodiceAreaTematica()));
+		}
+
+		if (richiestaServizio.getCodiceFiscaleDelegato() == null) {
+			map.put("codiceFiscaleDelegato", null);
+		}
+		else {
+			map.put(
+				"codiceFiscaleDelegato",
+				String.valueOf(richiestaServizio.getCodiceFiscaleDelegato()));
+		}
+
+		if (richiestaServizio.getCodiceFiscaleRichiedente() == null) {
+			map.put("codiceFiscaleRichiedente", null);
+		}
+		else {
+			map.put(
+				"codiceFiscaleRichiedente",
+				String.valueOf(
+					richiestaServizio.getCodiceFiscaleRichiedente()));
+		}
+
+		if (richiestaServizio.getCodiceIdentificativoServizio() == null) {
+			map.put("codiceIdentificativoServizio", null);
+		}
+		else {
+			map.put(
+				"codiceIdentificativoServizio",
+				String.valueOf(
+					richiestaServizio.getCodiceIdentificativoServizio()));
+		}
+
+		if (richiestaServizio.getDataAggiornamento() == null) {
+			map.put("dataAggiornamento", null);
+		}
+		else {
+			map.put(
+				"dataAggiornamento",
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataAggiornamento()));
+		}
+
+		if (richiestaServizio.getDataInserimento() == null) {
+			map.put("dataInserimento", null);
+		}
+		else {
+			map.put(
+				"dataInserimento",
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataInserimento()));
+		}
+
+		if (richiestaServizio.getDataProtocollo() == null) {
+			map.put("dataProtocollo", null);
+		}
+		else {
+			map.put(
+				"dataProtocollo",
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataProtocollo()));
+		}
+
+		if (richiestaServizio.getDataProtocolloEsterno() == null) {
+			map.put("dataProtocolloEsterno", null);
+		}
+		else {
+			map.put(
+				"dataProtocolloEsterno",
+				liferayToJSONDateFormat.format(
+					richiestaServizio.getDataProtocolloEsterno()));
+		}
+
+		if (richiestaServizio.getDenominazioneAreaTematica() == null) {
+			map.put("denominazioneAreaTematica", null);
+		}
+		else {
+			map.put(
+				"denominazioneAreaTematica",
+				String.valueOf(
+					richiestaServizio.getDenominazioneAreaTematica()));
+		}
+
+		if (richiestaServizio.getDenominazioneEnte() == null) {
+			map.put("denominazioneEnte", null);
+		}
+		else {
+			map.put(
+				"denominazioneEnte",
+				String.valueOf(richiestaServizio.getDenominazioneEnte()));
+		}
+
+		if (richiestaServizio.getDenominazioneRichiedente() == null) {
+			map.put("denominazioneRichiedente", null);
+		}
+		else {
+			map.put(
+				"denominazioneRichiedente",
+				String.valueOf(
+					richiestaServizio.getDenominazioneRichiedente()));
+		}
+
+		if (richiestaServizio.getDenominazioneServizio() == null) {
+			map.put("denominazioneServizio", null);
+		}
+		else {
+			map.put(
+				"denominazioneServizio",
+				String.valueOf(richiestaServizio.getDenominazioneServizio()));
+		}
+
+		if (richiestaServizio.getEmailRichiedente() == null) {
+			map.put("emailRichiedente", null);
+		}
+		else {
+			map.put(
+				"emailRichiedente",
+				String.valueOf(richiestaServizio.getEmailRichiedente()));
+		}
+
 		if (richiestaServizio.getId() == null) {
 			map.put("id", null);
 		}
 		else {
 			map.put("id", String.valueOf(richiestaServizio.getId()));
+		}
+
+		if (richiestaServizio.getIdProcedura() == null) {
+			map.put("idProcedura", null);
+		}
+		else {
+			map.put(
+				"idProcedura",
+				String.valueOf(richiestaServizio.getIdProcedura()));
+		}
+
+		if (richiestaServizio.getIdentificativoEnte() == null) {
+			map.put("identificativoEnte", null);
+		}
+		else {
+			map.put(
+				"identificativoEnte",
+				String.valueOf(richiestaServizio.getIdentificativoEnte()));
+		}
+
+		if (richiestaServizio.getIdentificativoProcesso() == null) {
+			map.put("identificativoProcesso", null);
+		}
+		else {
+			map.put(
+				"identificativoProcesso",
+				String.valueOf(richiestaServizio.getIdentificativoProcesso()));
+		}
+
+		if (richiestaServizio.getNote() == null) {
+			map.put("note", null);
+		}
+		else {
+			map.put("note", String.valueOf(richiestaServizio.getNote()));
+		}
+
+		if (richiestaServizio.getNumeroProtocollo() == null) {
+			map.put("numeroProtocollo", null);
+		}
+		else {
+			map.put(
+				"numeroProtocollo",
+				String.valueOf(richiestaServizio.getNumeroProtocollo()));
+		}
+
+		if (richiestaServizio.getNumeroProtocolloEsterno() == null) {
+			map.put("numeroProtocolloEsterno", null);
+		}
+		else {
+			map.put(
+				"numeroProtocolloEsterno",
+				String.valueOf(richiestaServizio.getNumeroProtocolloEsterno()));
+		}
+
+		if (richiestaServizio.getOggetto() == null) {
+			map.put("oggetto", null);
+		}
+		else {
+			map.put("oggetto", String.valueOf(richiestaServizio.getOggetto()));
+		}
+
+		if (richiestaServizio.getPartitaIvaRichiedente() == null) {
+			map.put("partitaIvaRichiedente", null);
+		}
+		else {
+			map.put(
+				"partitaIvaRichiedente",
+				String.valueOf(richiestaServizio.getPartitaIvaRichiedente()));
 		}
 
 		if (richiestaServizio.getStato() == null) {
@@ -121,10 +623,156 @@ public class RichiestaServizioSerDes {
 			RichiestaServizio richiestaServizio, String jsonParserFieldName,
 			Object jsonParserFieldValue) {
 
-			if (Objects.equals(jsonParserFieldName, "id")) {
+			if (Objects.equals(jsonParserFieldName, "codiceAreaTematica")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setCodiceAreaTematica(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "codiceFiscaleDelegato")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setCodiceFiscaleDelegato(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "codiceFiscaleRichiedente")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setCodiceFiscaleRichiedente(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "codiceIdentificativoServizio")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setCodiceIdentificativoServizio(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "dataAggiornamento")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDataAggiornamento(
+						toDate((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "dataInserimento")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDataInserimento(
+						toDate((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "dataProtocollo")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDataProtocollo(
+						toDate((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "dataProtocolloEsterno")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDataProtocolloEsterno(
+						toDate((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "denominazioneAreaTematica")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDenominazioneAreaTematica(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "denominazioneEnte")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDenominazioneEnte(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "denominazioneRichiedente")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDenominazioneRichiedente(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "denominazioneServizio")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setDenominazioneServizio(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "emailRichiedente")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setEmailRichiedente(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "id")) {
 				if (jsonParserFieldValue != null) {
 					richiestaServizio.setId(
 						Long.valueOf((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "idProcedura")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setIdProcedura(
+						Integer.valueOf((String)jsonParserFieldValue));
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "identificativoEnte")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setIdentificativoEnte(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "identificativoProcesso")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setIdentificativoProcesso(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "note")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setNote((String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "numeroProtocollo")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setNumeroProtocollo(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "numeroProtocolloEsterno")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setNumeroProtocolloEsterno(
+						(String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(jsonParserFieldName, "oggetto")) {
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setOggetto((String)jsonParserFieldValue);
+				}
+			}
+			else if (Objects.equals(
+						jsonParserFieldName, "partitaIvaRichiedente")) {
+
+				if (jsonParserFieldValue != null) {
+					richiestaServizio.setPartitaIvaRichiedente(
+						(String)jsonParserFieldValue);
 				}
 			}
 			else if (Objects.equals(jsonParserFieldName, "stato")) {
--- modules/servizi-digitali-rest-service/servizi-digitali-rest-service-impl/src/main/java/it/servizidigitali/restservice/internal/resource/v1_0/RichiesteServizioResourceImpl.java
+++ modules/servizi-digitali-rest-service/servizi-digitali-rest-service-impl/src/main/java/it/servizidigitali/restservice/internal/resource/v1_0/RichiesteServizioResourceImpl.java
@@ -1,89 +1,92 @@
 package it.servizidigitali.restservice.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.pagination.Page;
+
 import java.util.ArrayList;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Context;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
-
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.vulcan.pagination.Page;
 
 import it.servizidigitali.restservice.dto.v1_0.InsertRichiestaServizioRequest;
 import it.servizidigitali.restservice.dto.v1_0.RichiestaServizio;
 import it.servizidigitali.restservice.dto.v1_0.UpdateRichiestaServizioRequest;
+import it.servizidigitali.restservice.internal.converter.EntityToSchemaModelConverter;
 import it.servizidigitali.restservice.resource.v1_0.RichiesteServizioResource;
+import it.servizidigitali.scrivaniaoperatore.model.Richiesta;
+import it.servizidigitali.scrivaniaoperatore.service.RichiestaLocalService;
 
 /**
  * @author pindi
  */
-@Component(
-		properties = "OSGI-INF/liferay/rest/v1_0/richieste-servizio.properties",
-		scope = ServiceScope.PROTOTYPE, service = RichiesteServizioResource.class
-		)
-public class RichiesteServizioResourceImpl
-extends BaseRichiesteServizioResourceImpl {
+@Component(properties = "OSGI-INF/liferay/rest/v1_0/richieste-servizio.properties", scope = ServiceScope.PROTOTYPE, service = RichiesteServizioResource.class)
+public class RichiesteServizioResourceImpl extends BaseRichiesteServizioResourceImpl {
+
+	@Reference
+	private RichiestaLocalService richiestaLocalService;
+
+	@Reference
+	private EntityToSchemaModelConverter entityToSchemaModelConverter;
 
 	@Override
 	public Page<RichiestaServizio> searchRichiesteServizio(Integer page, Integer size, String q) {
 
 		ArrayList<RichiestaServizio> l = new ArrayList<>();
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			RichiestaServizio r = new RichiestaServizio();
 			r.setId(Long.valueOf(i));
-			
-			if(Validator.isNotNull(user)) {
+
+			if (Validator.isNotNull(user)) {
 				r.setStato(user.getScreenName());
 			}
-			
+
 			l.add(r);
 		}
 		return Page.of(l);
 
 	}
-	
-	@Override
-		public RichiestaServizio insertRichiestaServizio(InsertRichiestaServizioRequest insertRichiestaServizioRequest)
-				throws Exception {
-		
-			
-			RichiestaServizio r = new RichiestaServizio();
-			r.setId(99l);
-			
-			return r;
-		
-		}
 
 	@Override
-	public RichiestaServizio getRichiestaServizio( Long id) throws Exception {
+	public RichiestaServizio insertRichiestaServizio(InsertRichiestaServizioRequest insertRichiestaServizioRequest) throws Exception {
+
+		RichiestaServizio r = new RichiestaServizio();
+		r.setId(99l);
+
+		return r;
+
+	}
+
+	@Override
+	public RichiestaServizio getRichiestaServizio(Long id) throws Exception {
+
+		Richiesta richiesta = richiestaLocalService.getRichiesta(id);
+		RichiestaServizio richiestaServizio = entityToSchemaModelConverter.getRichiestaServizio(richiesta);
+
+		return richiestaServizio;
+	}
+
+	@Override
+	public RichiestaServizio updateRichiestaServizio(@NotNull Long id, UpdateRichiestaServizioRequest updateRichiestaServizioRequest) throws Exception {
 
 		RichiestaServizio r = new RichiestaServizio();
 		r.setId(99L);
 		return r;
+
 	}
 
 	@Override
-	public RichiestaServizio updateRichiestaServizio(@NotNull Long id,
-			UpdateRichiestaServizioRequest updateRichiestaServizioRequest) throws Exception {
-		
-		RichiestaServizio r = new RichiestaServizio();
-		r.setId(99L);
-		return r;
-		
-	}
-	
-	@Override
-	public RichiestaServizio patchRichiestaServizio(@NotNull Long id,
-			UpdateRichiestaServizioRequest updateRichiestaServizioRequest) throws Exception {
+	public RichiestaServizio patchRichiestaServizio(@NotNull Long id, UpdateRichiestaServizioRequest updateRichiestaServizioRequest) throws Exception {
 		RichiestaServizio r = new RichiestaServizio();
 		r.setId(99L);
 		r.setStato(updateRichiestaServizioRequest.getStato());
 		return r;
 	}
-	
+
 	@Context
 	private User user;
 
