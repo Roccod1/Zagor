cbb2c3b079171a28a824a162643e773e8a0f57cc
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-10-12 17:58:03 +0200

    Fix enumeration senza descrizione.

--- modules/gestione-pagamenti/gestione-pagamenti-common/src/main/java/it/servizidigitali/gestionepagamenti/common/enumeration/StatoPagamento.java
+++ modules/gestione-pagamenti/gestione-pagamenti-common/src/main/java/it/servizidigitali/gestionepagamenti/common/enumeration/StatoPagamento.java
@@ -1,32 +1,10 @@
 package it.servizidigitali.gestionepagamenti.common.enumeration;
 
-import java.util.stream.Stream;
-
 /**
  * @author pindi
  *
  */
 public enum StatoPagamento {
-	NUOVO("NUOVO"), IN_ATTESA("IN ATTESA"), CONFERMATO("CONFERMATO"), COMPLETATO("COMPLETATO"), ERRORE("ERRORE"), NON_CONFERMATO("NON CONFERMATO");
-	
-	private String descrizione;
-	
-	private StatoPagamento(String descrizione) {
-		this.descrizione = descrizione;
-	}
 
-	public String getDescrizione() {
-		return descrizione;
-	}
-	
-	public static String getDescrizioneByName(String name) {
-		
-		String descrizione = "-";
-		
-		if (Stream.of(StatoPagamento.values()).filter(en -> en.toString().equals(name)).findAny().isPresent()) {
-			descrizione = StatoPagamento.valueOf(name).getDescrizione();
-		}
-		
-		return descrizione;
-	}
+	NUOVO, IN_ATTESA, CONFERMATO, COMPLETATO, ERRORE, NON_CONFERMATO;
 }
