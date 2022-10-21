846d849ed4c4dd64b5a6c0e49892be2a72bdb07f
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-10-21 16:19:26 +0200

    Aggiunto modulo gestione-pagamenti-integration-common.

---
+++ modules/gestione-pagamenti/gestione-pagamenti-integration-common/src/main/java/it/servizidigitali/gestionepagamenti/integration/common/client/enumeration/StatoPagamento.java
@@ -0,0 +1,9 @@
+package it.servizidigitali.gestionepagamenti.integration.common.client.enumeration;
+
+/**
+ * @author pindi
+ *
+ */
+public enum StatoPagamento {
+	NON_INIZIATO, IN_CORSO, COMPLETATO, ANNULLATO, SCADUTO, ERRORE;
+}
---
+++ modules/gestione-pagamenti/gestione-pagamenti-integration-common/src/main/java/it/servizidigitali/gestionepagamenti/integration/common/client/enumeration/TipoPagamentiClient.java
@@ -0,0 +1,9 @@
+package it.servizidigitali.gestionepagamenti.integration.common.client.enumeration;
+
+/**
+ * @author pindi
+ *
+ */
+public enum TipoPagamentiClient {
+	MYPAY;
+}
