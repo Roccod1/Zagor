47135da89f866d61af7e570bffb9933d5fccdc72
Giancarlo Abrignani <giancarlo.abrignani@linksmt.it>
2022-10-25 13:12:48 +0200

    Implementazione iniziale verifica validita bollo digitale;

--- modules/gestione-pagamenti/verifica-ebollo-frontend/src/main/java/it/servizidigitali/verifica/ebollo/frontend/constants/VerificaEbolloFrontendPortletKeys.java
+++ modules/gestione-pagamenti/verifica-ebollo-frontend/src/main/java/it/servizidigitali/verifica/ebollo/frontend/constants/VerificaEbolloFrontendPortletKeys.java
@@ -7,4 +7,9 @@ public class VerificaEbolloFrontendPortletKeys {
 
 	public static final String VERIFICAEBOLLOPORTLET = "it_servizidigitali_verificaebollo_VerificaEbolloPortlet";
 
+	public static final String UPLOAD_FILES_ACTION_COMMAND = "/upload-files";
+	
+	public static final String UPLOAD_DOCUMENTO_ATTRIBUTE = "uploadDocumento";
+	
+	public static final String UPLOAD_XML_BOLLO_ATTRIBUTE = "uploadXmlBollo";
 }
