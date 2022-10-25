c57a9ac7659f23fa6c312f3a2fb60ccd75a34664
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-10-25 09:20:18 +0200

    Creato scheletro portlet verifica-ebollo-frontend.

---
+++ modules/gestione-pagamenti/verifica-ebollo-frontend/src/main/java/it/servizidigitali/verifica/ebollo/constants/VerificaEbolloPorteltPortletKeys.java
@@ -0,0 +1,11 @@
+package it.servizidigitali.verifica.ebollo.constants;
+
+/**
+ * @author pindi
+ */
+public class VerificaEbolloPorteltPortletKeys {
+
+	public static final String VERIFICAEBOLLOPORTELT =
+		"it_servizidigitali_gestionepagamenti_verificaebollo_VerificaEbolloPorteltPortlet";
+
+}
---
+++ modules/gestione-pagamenti/verifica-ebollo-frontend/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="verificaebolloportelt.caption"/></b>
+</p>
