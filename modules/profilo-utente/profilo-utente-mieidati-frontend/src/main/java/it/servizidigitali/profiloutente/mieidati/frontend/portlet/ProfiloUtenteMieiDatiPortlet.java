e777f841f192d4d86294e47cd0125c90ec46b5eb
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-10-26 10:29:40 +0200

    Aggiunta selezione tab da parametro in querystring.

--- modules/profilo-utente/profilo-utente-mieidati-frontend/src/main/java/it/servizidigitali/profiloutente/mieidati/frontend/portlet/ProfiloUtenteMieiDatiPortlet.java
+++ modules/profilo-utente/profilo-utente-mieidati-frontend/src/main/java/it/servizidigitali/profiloutente/mieidati/frontend/portlet/ProfiloUtenteMieiDatiPortlet.java
@@ -12,6 +12,7 @@ import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.IOException;
@@ -22,6 +23,7 @@ import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -35,79 +37,83 @@ import it.servizidigitali.profiloutente.mieidati.frontend.constants.ProfiloUtent
 /**
  * @author filierim
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.servizidigitali",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=ProfiloUtenteMieiDati",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + ProfiloUtenteMieiDatiPortletKeys.PROFILOUTENTEMIEIDATI,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+@Component(//
+		immediate = true, //
+		property = { //
+				"com.liferay.portlet.display-category=category.servizidigitali", //
+				"com.liferay.portlet.header-portlet-css=/css/main.css", //
+				"com.liferay.portlet.instanceable=true", //
+				"javax.portlet.display-name=ProfiloUtenteMieiDati", //
+				"javax.portlet.init-param.template-path=/", //
+				"javax.portlet.init-param.view-template=/view.jsp", //
+				"javax.portlet.name=" + ProfiloUtenteMieiDatiPortletKeys.PROFILOUTENTEMIEIDATI, //
+				"javax.portlet.resource-bundle=content.Language", //
+				"javax.portlet.security-role-ref=power-user,user"//
+		}, //
+		service = Portlet.class//
+) //
 public class ProfiloUtenteMieiDatiPortlet extends MVCPortlet {
 
 	private static final Log _log = LogFactoryUtil.getLog(ProfiloUtenteMieiDatiPortlet.class);
-	
+
 	@Reference
 	private AnagrafeIntegrationFactoryService anagrafeIntegrationFactoryService;
-	
+
 	@Reference
 	private OrganizationLocalService organizationLocalService;
-	
+
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-		//pulisco gli errori precedenti 
+		// pulisco gli errori precedenti
 		SessionErrors.clear(renderRequest);
 		String tabAttiva = ParamUtil.getString(renderRequest, ProfiloUtenteMieiDatiPortletKeys.TAB_ATTIVA);
+		if (Validator.isNull(tabAttiva)) {
+			HttpServletRequest httpServletRequest = PortalUtil.getOriginalServletRequest(PortalUtil.getHttpServletRequest(renderRequest));
+			tabAttiva = ParamUtil.getString(httpServletRequest, ProfiloUtenteMieiDatiPortletKeys.TAB_ATTIVA);
+		}
 		ServiceContext serviceContext = null;
 		ThemeDisplay themeDisplay = null;
-		
+
 		User utenteCorrente = null;
 		DatiAnagrafici datiAnagrafici = null;
 		Organization organization = null;
 		boolean anpr = false;
-		
+
 		List<DatiAnagrafici.ComponenteNucleoFamiliare> listaComponentiNucleoFamiliare = new ArrayList<DatiAnagrafici.ComponenteNucleoFamiliare>();
 		try {
 			serviceContext = ServiceContextFactory.getInstance(renderRequest);
 			themeDisplay = serviceContext.getThemeDisplay();
 			utenteCorrente = themeDisplay.getUser();
-            User user = themeDisplay.getUser();
-            // recupero il siteGroupOrganizationId dal siteGroup
-            Long siteGroupOrganizationId = themeDisplay.getSiteGroup().getOrganizationId();
+			User user = themeDisplay.getUser();
+			// recupero il siteGroupOrganizationId dal siteGroup
+			Long siteGroupOrganizationId = themeDisplay.getSiteGroup().getOrganizationId();
 
-            AnagrafeIntegrationService anagrafeIntegrationService = anagrafeIntegrationFactoryService.getAnagrafeIntegrationService(siteGroupOrganizationId);
+			AnagrafeIntegrationService anagrafeIntegrationService = anagrafeIntegrationFactoryService.getAnagrafeIntegrationService(siteGroupOrganizationId);
 
-            organization = organizationLocalService.fetchOrganization(siteGroupOrganizationId);
-            if(Validator.isNotNull(organization)) {
-            	ExpandoBridge expandoOrganization = organization.getExpandoBridge();
-            	anpr = (boolean) expandoOrganization.getAttribute(ProfiloUtenteMieiDatiPortletKeys.EXPANDO_ORGANIZATION_ANPR);
-            	
-            	if(!anpr) {
-            		renderRequest.setAttribute(ProfiloUtenteMieiDatiPortletKeys.NOME_FONTE, organization.getName());
-            	}
-            }
+			organization = organizationLocalService.fetchOrganization(siteGroupOrganizationId);
+			if (Validator.isNotNull(organization)) {
+				ExpandoBridge expandoOrganization = organization.getExpandoBridge();
+				anpr = (boolean) expandoOrganization.getAttribute(ProfiloUtenteMieiDatiPortletKeys.EXPANDO_ORGANIZATION_ANPR);
 
-            datiAnagrafici = anagrafeIntegrationService.getDatiAnagrafici(user.getScreenName(), siteGroupOrganizationId, null);
-            if (datiAnagrafici != null && datiAnagrafici.getComponentiNucleoFamiliare() != null) {
-                _log.info("datiAnagrafici: " + datiAnagrafici.getComponentiNucleoFamiliare().size());
-                listaComponentiNucleoFamiliare = datiAnagrafici.getComponentiNucleoFamiliare();
-            }
-        }
-		catch (BackofficeIntegrationClientException e) {
-            _log.error("render :: " + e.getMessage(), e);
-            SessionErrors.add(renderRequest, ProfiloUtenteMieiDatiPortletKeys.ERRORE_BACKOFFICE);
+				if (!anpr) {
+					renderRequest.setAttribute(ProfiloUtenteMieiDatiPortletKeys.NOME_FONTE, organization.getName());
+				}
+			}
+
+			datiAnagrafici = anagrafeIntegrationService.getDatiAnagrafici(user.getScreenName(), siteGroupOrganizationId, null);
+			if (datiAnagrafici != null && datiAnagrafici.getComponentiNucleoFamiliare() != null) {
+				_log.info("datiAnagrafici: " + datiAnagrafici.getComponentiNucleoFamiliare().size());
+				listaComponentiNucleoFamiliare = datiAnagrafici.getComponentiNucleoFamiliare();
+			}
 		}
-        catch (Exception e) {
-            _log.error("render :: " + e.getMessage(), e);
-        }
-		
+		catch (BackofficeIntegrationClientException e) {
+			_log.error("render :: " + e.getMessage(), e);
+			SessionErrors.add(renderRequest, ProfiloUtenteMieiDatiPortletKeys.ERRORE_BACKOFFICE);
+		}
+		catch (Exception e) {
+			_log.error("render :: " + e.getMessage(), e);
+		}
+
 		renderRequest.setAttribute(ProfiloUtenteMieiDatiPortletKeys.TAB_ATTIVA, tabAttiva);
 		renderRequest.setAttribute(ProfiloUtenteMieiDatiPortletKeys.COMPONENTI_NUCLEO_FAMILIARE, listaComponentiNucleoFamiliare);
 		renderRequest.setAttribute(ProfiloUtenteMieiDatiPortletKeys.UTENTE, utenteCorrente);
