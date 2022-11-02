cb20c3fe157a98025e54ad3bb8b08dda00437a28
Zonnog <giacomo.zonno@linksmt.it>
2022-11-02 13:08:50 +0100

    .

--- modules/presentatore-forms/presentatore-forms-common/src/main/java/it/servizidigitali/presentatoreforms/common/service/integration/enumeration/BackofficeServiceExceptionLanguageCode.java
+++ modules/presentatore-forms/presentatore-forms-common/src/main/java/it/servizidigitali/presentatoreforms/common/service/integration/enumeration/BackofficeServiceExceptionLanguageCode.java
@@ -17,7 +17,6 @@ public enum BackofficeServiceExceptionLanguageCode {
 	SOGGETTO_NON_VEDOVO("backoffice-service-exception-condizione-vedovanza-non-verificata"), //
 	SOGGETTO_NON_IN_VITA("backoffice-service-exception-condizione-in-vita-non-verificata"), //
 	ATTO_NON_PRESENTE("error.atto.non.presente"), //
-	
 	SOGGETTO_DECEDUTO_CERTIFICATO_NON_DISPONIBILE("backoffice-service-exception-condizione-deceduto-messaggio-generico");
 
 	private String liferayLanguageKey;
