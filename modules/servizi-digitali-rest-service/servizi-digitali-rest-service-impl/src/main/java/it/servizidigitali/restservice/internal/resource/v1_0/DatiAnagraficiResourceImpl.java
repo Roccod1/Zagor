3284d53ae55b43fa47741975fbda49bf69bc5a5a
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-11-08 15:09:18 +0100

    Aggiunta gestione errore 404 per RS get componenti nucleo.

--- modules/servizi-digitali-rest-service/servizi-digitali-rest-service-impl/src/main/java/it/servizidigitali/restservice/internal/resource/v1_0/DatiAnagraficiResourceImpl.java
+++ modules/servizi-digitali-rest-service/servizi-digitali-rest-service-impl/src/main/java/it/servizidigitali/restservice/internal/resource/v1_0/DatiAnagraficiResourceImpl.java
@@ -1,18 +1,19 @@
 package it.servizidigitali.restservice.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.vulcan.pagination.Page;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.NotFoundException;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
-
-import com.liferay.portal.kernel.model.Organization;
-import com.liferay.portal.vulcan.pagination.Page;
 
 import it.servizidigitali.backoffice.integration.model.anagrafe.DatiAnagrafici;
 import it.servizidigitali.backoffice.integration.model.anagrafe.DatiAnagrafici.ComponenteNucleoFamiliare;
@@ -27,45 +28,44 @@ import it.servizidigitali.restservice.resource.v1_0.DatiAnagraficiResource;
 /**
  * @author pindi
  */
-@Component(
-		properties = "OSGI-INF/liferay/rest/v1_0/dati-anagrafici.properties",
-		scope = ServiceScope.PROTOTYPE, service = DatiAnagraficiResource.class
-		)
+@Component(properties = "OSGI-INF/liferay/rest/v1_0/dati-anagrafici.properties", scope = ServiceScope.PROTOTYPE, service = DatiAnagraficiResource.class)
 public class DatiAnagraficiResourceImpl extends BaseDatiAnagraficiResourceImpl {
 
 	@Override
-	public Page<ComponenteNucleoFamiliareLight> getComponentiNucleoFamiliare(@NotNull String userToken,
-			String nomeComune) throws Exception {
+	public Page<ComponenteNucleoFamiliareLight> getComponentiNucleoFamiliare(@NotNull String userToken, String nomeComune) throws Exception {
 
 		try {
 			Map<String, Object> claims = _jwtUtilityService.verifyJwtAndGetClaims(userToken);
-			String codiceFiscale = (String)claims.get(JWTUtilityConstant.CLAIM_USERNAME);
+			String codiceFiscale = (String) claims.get(JWTUtilityConstant.CLAIM_USERNAME);
 
 			Organization organization = _organizationUtility.getByName(nomeComune);
 
-			DatiAnagrafici datiAnagrafici = _anagrafeIntegrationFactoryService.getAnagrafeIntegrationService(organization.getOrganizationId())
-					.getDatiAnagrafici(codiceFiscale, organization.getOrganizationId(), null);
+			DatiAnagrafici datiAnagrafici = _anagrafeIntegrationFactoryService.getAnagrafeIntegrationService(organization.getOrganizationId()).getDatiAnagrafici(codiceFiscale,
+					organization.getOrganizationId(), null);
 
 			List<ComponenteNucleoFamiliareLight> res = new ArrayList<>();
-			List<ComponenteNucleoFamiliare> componenti = datiAnagrafici
-					.getComponentiNucleoFamiliare();
+			List<ComponenteNucleoFamiliare> componenti = datiAnagrafici.getComponentiNucleoFamiliare();
 			if (componenti != null) {
-				componenti.forEach( c -> {
+				componenti.forEach(c -> {
 					ComponenteNucleoFamiliareLight l = new ComponenteNucleoFamiliareLight();
 					l.setCodiceFiscale(c.getCodiceFiscale());
 					l.setCognome(c.getCognome());
 					l.setDataNascita(c.getDataNascita());
 					l.setNome(c.getNome());
 					l.setSesso(c.getSesso());
-	
+
 					res.add(l);
 				});
+			}
+			else {
+				throw new NotFoundException("Lista componenti nucleo familiare vuota");
 			}
 
 			Page<ComponenteNucleoFamiliareLight> page = Page.of(res);
 
 			return page;
-		}catch (JwtException e) {
+		}
+		catch (JwtException e) {
 			throw new ForbiddenException(e.getDescription());
 		}
 	}
