a2be522a0c3a739ab40b5ad711cd91744f6141d2
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-11-16 14:53:23 +0100

    Aggiunti custom attributes tipoLogin e loginSenzaCodiceFiscale.

--- modules/servizi-digitali-common/servizi-digitali-common-utility/src/main/java/it/servizidigitali/common/utility/enumeration/UserCustomAttributes.java
+++ modules/servizi-digitali-common/servizi-digitali-common-utility/src/main/java/it/servizidigitali/common/utility/enumeration/UserCustomAttributes.java
@@ -6,8 +6,16 @@ package it.servizidigitali.common.utility.enumeration;
  */
 public enum UserCustomAttributes {
 
-	ACCETTA_PRIVACY("accettaPrivacy"), EMAIL_AGGIUNTIVA("emailAggiuntiva"), LIVELLO_SPID("livelloSPID"), PEC("pec"), RESIDENZA("residenza"), TELEFONO("telefono"), SPID_CODE(
-			"spidCode"), LOGIN_SENZA_EMAIL("loginSenzaEmail");
+	ACCETTA_PRIVACY("accettaPrivacy"), //
+	EMAIL_AGGIUNTIVA("emailAggiuntiva"), //
+	LIVELLO_SPID("livelloSPID"), //
+	PEC("pec"), //
+	RESIDENZA("residenza"), //
+	TELEFONO("telefono"), //
+	SPID_CODE("spidCode"), //
+	LOGIN_SENZA_EMAIL("loginSenzaEmail"), //
+	LOGIN_SENZA_CODICE_FISCALE("loginSenzaCodiceFiscale"), //
+	TIPO_LOGIN("tipoLogin");
 
 	private String nomeAttributo;
 
