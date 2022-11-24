a6ec506804e69c39695dbb29bdd44429b63f2fd1
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2022-11-24 09:50:34 +0100

    Aggiunto scheletro modulo portlet accreditamento enti.

---
+++ modules/accreditamento-enti/accreditamento-enti-frontend/src/main/java/it/servizidigitali/accreditamentoenti/frontend/constants/AccreditamentoEntiFrontendPortletKeys.java
@@ -0,0 +1,10 @@
+package it.servizidigitali.accreditamentoenti.frontend.constants;
+
+/**
+ * @author pindi
+ */
+public class AccreditamentoEntiFrontendPortletKeys {
+
+	public static final String ACCREDITAMENTOENTIFRONTEND = "it_servizidigitali_accreditamentoenti_frontend_AccreditamentoEntiFrontendPortlet";
+
+}
---
+++ modules/accreditamento-enti/accreditamento-enti-frontend/src/main/java/it/servizidigitali/accreditamentoenti/frontend/portlet/AccreditamentoEntiFrontendPortlet.java
@@ -0,0 +1,30 @@
+package it.servizidigitali.accreditamentoenti.frontend.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+import it.servizidigitali.accreditamentoenti.frontend.constants.AccreditamentoEntiFrontendPortletKeys;
+
+/**
+ * @author pindi
+ */
+@Component(//
+		immediate = true, //
+		property = { //
+				"com.liferay.portlet.display-category=category.servizidigitali", //
+				"com.liferay.portlet.header-portlet-css=/css/main.css", //
+				"com.liferay.portlet.instanceable=true", //
+				"javax.portlet.display-name=AccreditamentoEntiFrontend", //
+				"javax.portlet.init-param.template-path=/", //
+				"javax.portlet.init-param.view-template=/view.jsp", //
+				"javax.portlet.name=" + AccreditamentoEntiFrontendPortletKeys.ACCREDITAMENTOENTIFRONTEND, //
+				"javax.portlet.resource-bundle=content.Language", //
+				"javax.portlet.security-role-ref=power-user,user"//
+		}, //
+		service = Portlet.class//
+) //
+public class AccreditamentoEntiFrontendPortlet extends MVCPortlet {
+}
