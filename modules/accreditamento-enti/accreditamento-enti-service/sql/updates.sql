cef4d12944a4e61142137ba4339bb30fcaeb2693
Gianluca Pindinelli <gianluca.pindinelli@linksmt.it>
2023-01-25 14:25:33 +0100

    Spostati script SQL in file esterno.

---
+++ modules/accreditamento-enti/accreditamento-enti-service/sql/updates.sql
@@ -0,0 +1,34 @@
+CREATE TABLE ente (
+  uuid_ varchar(75) DEFAULT NULL,
+  enteId bigint NOT NULL,
+  groupId bigint DEFAULT NULL,
+  companyId bigint DEFAULT NULL,
+  userId bigint DEFAULT NULL,
+  userName varchar(75) DEFAULT NULL,
+  createDate datetime(6) DEFAULT NULL,
+  modifiedDate datetime(6) DEFAULT NULL,
+  denominazione varchar(75) NOT NULL,
+  strutturaRiferimento varchar (250) DEFAULT NULL,
+  sitoWeb varchar (250) DEFAULT NULL,
+  tipo varchar(75) NOT NULL,
+  codiceIPA varchar(6) NOT NULL,
+  pec varchar(75) NOT NULL,
+
+  PRIMARY KEY (enteId)
+);
+
+CREATE TABLE responsabile_ente (
+  uuid_ varchar(75) DEFAULT NULL,
+  responsabileEnteId bigint NOT NULL,
+  responsabileUserId  bigint NOT NULL,
+  enteId bigint NOT NULL,
+  groupId bigint DEFAULT NULL,
+  companyId bigint DEFAULT NULL,
+  userId bigint DEFAULT NULL,
+  userName varchar(75) DEFAULT NULL,
+  createDate datetime(6) DEFAULT NULL,
+  modifiedDate datetime(6) DEFAULT NULL,
+
+  PRIMARY KEY (responsabileEnteId),
+  FOREIGN KEY (enteId) REFERENCES ente(enteId)
+);
